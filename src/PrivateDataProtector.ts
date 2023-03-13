0a0b5c98b685875f5ab1eb21ddcea695c2f6b473
SeddikBellamine <bellamine.seddik@gmail.com>
2023-03-13 07:14:19 +0100

    add the PrivateDataProtector sdk
    *implement the sdk class

---
+++ src/PrivateDataProtector.ts
@@ -0,0 +1,33 @@
+import { Web3Provider } from '@ethersproject/providers';
+import { IExec } from 'iexec';
+import { createCNFT } from './confidentialNFT';
+import { Observable } from './reactive';
+
+export default class IExecPrivateDataProtector {
+  createCNFT: (
+    data: string | ArrayBuffer | Uint8Array | Buffer,
+    name: string
+  ) => Observable;
+
+  constructor(
+    ethProvider: any,
+    { ipfsGateway, providerOptions = {}, iexecOptions = {} }: any = {}
+  ) {
+    let iexec: any;
+    let ethersProvider: any;
+    try {
+      iexec = new IExec(
+        { ethProvider },
+        { confirms: 3, providerOptions, ...iexecOptions }
+      );
+      ethersProvider = ethProvider.provider || new Web3Provider(ethProvider);
+    } catch (e) {
+      throw Error('Unsupported ethProvider');
+    }
+
+    this.createCNFT = (
+      data: string | ArrayBuffer | Uint8Array | Buffer,
+      name: string
+    ) => createCNFT({ data, name, iexec, ipfsGateway });
+  }
+}
