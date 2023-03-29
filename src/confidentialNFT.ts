c4b2a9f33425c8a9acd2703de82ea75c5674232d
SeddikBellamine <bellamine.seddik@gmail.com>
2023-03-29 04:21:05 +0200

    remove logic duplication

--- src/confidentialNFT.ts
+++ src/confidentialNFT.ts
@@ -1,7 +1,6 @@
 import { Buffer } from 'buffer';
 import { DEFAULT_IEXEC_IPFS_NODE_MULTIADDR } from './conf';
-import { WorkflowError } from './errors';
-import { add } from './ipfs-service';
+import { createCNFTWithObservable } from './confidentialNFTWithObservable';
 import { throwIfMissing } from './validators';
 
 const createCNFT = ({
@@ -14,66 +13,37 @@ const createCNFT = ({
   data: string | ArrayBuffer | Uint8Array | Buffer;
   name: string;
   ipfsNodeMultiaddr?: string;
-}): Promise<any> =>
-  new Promise(function (resolve, reject) {
-    const start = async () => {
-      try {
-        const encryptionKey = iexec.dataset.generateEncryptionKey();
-        let confidentialNFTdata = data;
-        if (typeof data === 'string') {
-          confidentialNFTdata = Buffer.from(data, 'utf8');
+}): Promise<any> => {
+  let cNFTAddress;
+  let encryptionKey;
+  let Ipfsmultiaddr;
+  const promise = new Promise((resolve, reject) => {
+    createCNFTWithObservable({
+      iexec,
+      data,
+      name,
+      ipfsNodeMultiaddr,
+    }).subscribe(
+      (data) => {
+        const { message } = data;
+        switch (message) {
+          case 'ENCRYPTION_KEY_CREATED':
+            encryptionKey = data.encryptionKey;
+            break;
+          case 'ENCRYPTED_FILE_UPLOADED':
+            Ipfsmultiaddr = data.multiaddr;
+            break;
+          case 'CONFIDENTIAL_NFT_DEPLOYMENT_SUCCESS':
+            cNFTAddress = data.address;
+            break;
+          default:
         }
-        const encryptedFile = await iexec.dataset
-          .encrypt(confidentialNFTdata, encryptionKey)
-          .catch((e) => {
-            throw new WorkflowError('Failed to encrypt data', e);
-          });
-        const checksum = await iexec.dataset
-          .computeEncryptedFileChecksum(encryptedFile)
-          .catch((e) => {
-            throw new WorkflowError(
-              'Failed to compute encrypted data checksum',
-              e
-            );
-          });
-
-        const cid = await add(encryptedFile, { ipfsNodeMultiaddr }).catch(
-          (e) => {
-            throw new WorkflowError('Failed to upload encrypted data', e);
-          }
-        );
-        const multiaddr = `/ipfs/${cid}`;
-        const { address, txHash } = await iexec.dataset
-          .deployDataset({
-            owner: await iexec.wallet.getAddress(),
-            name: name,
-            multiaddr,
-            checksum,
-          })
-          .catch((e) => {
-            throw new WorkflowError('Failed to deploy confidential NFT', e);
-          });
-
-        await iexec.dataset
-          .pushDatasetSecret(address, encryptionKey)
-          .catch((e: any) => {
-            throw new WorkflowError(
-              'Failed to push API confidential NFT encryption key',
-              e
-            );
-          });
-        const cNFTAddress = address;
-        const Ipfsmultiaddr = multiaddr;
-        resolve({ cNFTAddress, encryptionKey, Ipfsmultiaddr });
-      } catch (e: any) {
-        if (e instanceof WorkflowError) {
-          reject(e);
-        } else {
-          reject(new WorkflowError('CNFT creation unexpected error', e));
-        }
-      }
-    };
-
-    start();
+      },
+      (e) => reject(e),
+      () => resolve({ cNFTAddress, encryptionKey, Ipfsmultiaddr })
+    );
   });
+
+  return promise;
+};
 export { createCNFT };
