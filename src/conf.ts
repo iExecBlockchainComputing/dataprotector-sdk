df2c4dcd045e0a40bca2ec6a01f0c61a56bdc583
SeddikBellamine <bellamine.seddik@gmail.com>
2023-04-13 15:35:01 +0200

    add smart contract interaction configuration & ABI

--- src/conf.ts
+++ src/conf.ts
@@ -1,3 +1,14 @@
 const DEFAULT_IPFS_GATEWAY = 'https://ipfs.iex.ec';
 const DEFAULT_IEXEC_IPFS_NODE_MULTIADDR = '/dns4/ipfs-upload.iex.ec/https/';
-export { DEFAULT_IPFS_GATEWAY, DEFAULT_IEXEC_IPFS_NODE_MULTIADDR };
+const NETWORK = {
+  chainId: 134,
+  name: 'iExec Sidechain',
+  url: 'https://bellecour.iex.ec',
+};
+const CONTRACT_ADDRESS = '0x608e5A4514C184c5Cb043026C87787c98061DA6e';
+export {
+  NETWORK,
+  DEFAULT_IPFS_GATEWAY,
+  DEFAULT_IEXEC_IPFS_NODE_MULTIADDR,
+  CONTRACT_ADDRESS,
+};
