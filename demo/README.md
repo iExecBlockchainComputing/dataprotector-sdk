7d877766d8162cbcb3079480027ea1ab32e43774
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-04-26 13:14:28 +0200

    add demos code in various contexts

---
+++ demo/README.md
@@ -0,0 +1,24 @@
+# Demo
+
+dataprotector-sdk demos
+
+## Usage
+
+pick a demo
+
+```sh
+# node typescript demo for example
+cd ts-node
+```
+
+install deps
+
+```sh
+npm i
+```
+
+run the demo
+
+```sh
+npm start
+```
---
+++ demo/browser-webpack/src/index.js
@@ -0,0 +1,30 @@
+import { IExecDataProtector } from 'dataprotector-sdk';
+
+const main = async () => {
+  if (!window.ethereum) {
+    throw Error('missing injected ethereum provider in page');
+  }
+
+  await window.ethereum.request({
+    method: 'eth_requestAccounts',
+  });
+  const dataProtector = new IExecDataProtector(window.ethereum);
+
+  dataProtector
+    .protectDataObservable({
+      data: {
+        firstName: 'John',
+        familyName: 'Doe',
+        birthYear: 1971,
+        usCitizen: true,
+      },
+      name: 'my personal data',
+    })
+    .subscribe(
+      (data) => console.log(data),
+      (e) => console.log(e),
+      () => console.log('DONE')
+    );
+};
+
+main();
