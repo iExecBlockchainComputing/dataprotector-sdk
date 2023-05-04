5a8b03b90e0ed41cae7306bea9fd079ce15624e4
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-05-04 08:41:00 +0200

    grantAccess input validation

--- src/dataProtector/grantAccess.ts
+++ src/dataProtector/grantAccess.ts
@@ -1,23 +1,47 @@
 import { GrantAccessParams, IExecConsumer } from './types.js';
 import { WorkflowError } from '../utils/errors.js';
-import { throwIfMissing } from '../utils/validators.js';
+import {
+  addressOrEnsSchema,
+  positiveIntegerStringSchema,
+  throwIfMissing,
+} from '../utils/validators.js';
 import { fetchGrantedAccess } from './fetchGrantedAccess.js';
 
 export const grantAccess = async ({
   iexec = throwIfMissing(),
-  protectedData = throwIfMissing(),
-  authorizedApp = throwIfMissing(),
-  authorizedUser = throwIfMissing(),
+  protectedData,
+  authorizedApp,
+  authorizedUser,
   pricePerAccess,
   numberOfAccess,
   tag,
 }: IExecConsumer & GrantAccessParams): Promise<string> => {
+  const vProtectedData = addressOrEnsSchema()
+    .required()
+    .label('protectedData')
+    .validateSync(protectedData);
+  const vAuthorizedApp = addressOrEnsSchema()
+    .required()
+    .label('authorizedApp')
+    .validateSync(authorizedApp);
+  const vAuthorizedUser = addressOrEnsSchema()
+    .required()
+    .label('authorizedUser')
+    .validateSync(authorizedUser);
+  const vPricePerAccess = positiveIntegerStringSchema()
+    .label('pricePerAccess')
+    .validateSync(pricePerAccess);
+  const vNumberOfAccess = positiveIntegerStringSchema()
+    .label('numberOfAccess')
+    .validateSync(numberOfAccess);
+  const vTag = tag; // not validated, will be removed in near future
+
   try {
     const publishedDatasetOrders = await fetchGrantedAccess({
       iexec,
-      protectedData,
-      authorizedApp,
-      authorizedUser,
+      protectedData: vProtectedData,
+      authorizedApp: vAuthorizedApp,
+      authorizedUser: vAuthorizedUser,
     });
     if (publishedDatasetOrders.length > 0) {
       throw new Error(
@@ -25,12 +49,12 @@ export const grantAccess = async ({
       );
     }
     const datasetorderTemplate = await iexec.order.createDatasetorder({
-      dataset: protectedData,
-      apprestrict: authorizedApp,
-      requesterrestrict: authorizedUser,
-      datasetprice: pricePerAccess,
-      volume: numberOfAccess,
-      tag: tag,
+      dataset: vProtectedData,
+      apprestrict: vAuthorizedApp,
+      requesterrestrict: vAuthorizedUser,
+      datasetprice: vPricePerAccess,
+      volume: vNumberOfAccess,
+      tag: vTag,
     });
     const datasetorder = await iexec.order.signDatasetorder(
       datasetorderTemplate
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,5 +1,46 @@
-import { ValidationError } from 'yup';
+import { utils } from 'ethers';
+import { ValidationError, string } from 'yup';
+
+const { isAddress } = utils;
 
 export const throwIfMissing = (): never => {
   throw new ValidationError('Missing parameter');
 };
+
+const isAddressTest = (value: string) => isAddress(value);
+const isEnsTest = (value: string) => value.endsWith('.eth') && value.length > 7;
+const isAnyTest = (value: string) => value === 'any';
+
+const isPositiveIntegerStringTest = (value: string) => /^\d+$/.test(value);
+
+export const addressSchema = () =>
+  string()
+    .transform((value: string) => value.toLowerCase())
+    .test('is-address', '${path} should be an ethereum address', (value) =>
+      isAddressTest(value)
+    );
+
+export const addressOrEnsSchema = () =>
+  string()
+    .transform((value: string) => value.toLowerCase())
+    .test(
+      'is-address-or-ens',
+      '${path} should be an ethereum address or a ENS name',
+      (value) => isAddressTest(value) || isEnsTest(value)
+    );
+
+export const addressOrEnsOrAnySchema = () =>
+  string()
+    .transform((value: string) => value.toLowerCase())
+    .test(
+      'is-address-or-ens-or-any',
+      '${path} should be an ethereum address, a ENS name, or "any"',
+      (value) => isAnyTest(value) || isAddressTest(value) || isEnsTest(value)
+    );
+
+export const positiveIntegerStringSchema = () =>
+  string().test(
+    'is-positive-int',
+    '${path} should be a positive integer',
+    (value) => isPositiveIntegerStringTest(value)
+  );
