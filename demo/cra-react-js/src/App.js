e4359c9df901fd028bae78f0fdbdadd02b3e0d99
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-06-08 15:48:10 +0200

    update cra demo

---
+++ demo/cra-react-js/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { test } from './dataprotector.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button
+          onClick={() => {
+            test().catch(console.log);
+          }}
+        >
+          TEST
+        </button>
+        <p>open the console to see logs</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
