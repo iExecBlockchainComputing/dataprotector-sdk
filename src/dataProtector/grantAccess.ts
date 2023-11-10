024f6bc265e188a885f7db99c774bf42ada900d7
luisccastillo <luiscarloscastillo@protonmail.com>
2023-11-10 11:32:05 +0100

    reorganize method in utils file

--- src/dataProtector/grantAccess.ts
+++ src/dataProtector/grantAccess.ts
@@ -9,38 +9,7 @@ import {
 } from '../utils/validators.js';
 import { fetchGrantedAccess } from './fetchGrantedAccess.js';
 import { GrantAccessParams, GrantedAccess, IExecConsumer } from './types.js';
-import { IExec } from 'iexec';
-import { id } from 'ethers';
-
-// Define named constants for function selectors
-const ADD_RESOURCE_SELECTOR = 'addResourceToWhitelist(address)';
-const KEY_PURPOSE_SELECTOR = 'keyHasPurpose(bytes32,uint256)';
-
-/**
- * Checks if a contract at the given address contains specific function selectors in its bytecode.
- *
- * @param iexec - The IExec instance.
- * @param appAddressOrWhitelist - The address of the contract to check.
- * @returns True if the contract contains the required function selectors; false otherwise.
- */
-export const isDeployedWhitelist = async (
-  iexec: IExec,
-  whitelistAddress: string
-): Promise<boolean> => {
-  const contractClient = await iexec.config.resolveContractsClient();
-  // Fetch the contract's bytecode
-  const contractBytecode = await contractClient.provider.getCode(
-    whitelistAddress
-  );
-  // Check if both function selectors are present in the bytecode
-  const hasAddResourceSelector = contractBytecode.includes(
-    id(ADD_RESOURCE_SELECTOR).substring(2, 10)
-  );
-  const hasKeyPurposeSelector = contractBytecode.includes(
-    id(KEY_PURPOSE_SELECTOR).substring(2, 10)
-  );
-  return hasAddResourceSelector && hasKeyPurposeSelector;
-};
+import { isDeployedWhitelist } from '../utils/whitelist.js';
 
 export const inferTagFromAppMREnclave = (mrenclave: string) => {
   const tag = ['tee'];
