e4d8fab56cfa30913c629f0c6f800dc915b82ce5
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2023-12-08 14:41:42 +0100

    Feature/review demos (#99)
    * Explicit expected node version
    
    * Simple log to indicate start of protectData()
    
    * Rename demo folder to "node-ts" + use tsx to run
    
    * Simplify styles + bump all deps + fix ESLint
    
    * Simplify styles + bump all deps + make it look the same as the React demo
    
    * Minor: rename demo folder to "node-ts" + some wording

--- demo/vite-vue/src/App.vue
+++ demo/vite-vue/src/App.vue
@@ -3,40 +3,24 @@ import { test } from './dataprotector';
 </script>
 
 <template>
-  <header>
+  <div>
     <img
       alt="Vue logo"
       class="logo"
       src="./assets/logo.svg"
-      width="125"
-      height="125"
+      width="100"
+      height="100"
     />
     <div>
       <button @click="test">TEST</button>
       <p>Open the console to see logs</p>
     </div>
-  </header>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 }
 </style>
