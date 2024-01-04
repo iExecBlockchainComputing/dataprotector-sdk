0194645d802c18cbd1be938fb919656a9aa466e9
Abbes Benayache <benayache.abbes@gmail.com>
2024-01-04 18:26:43 +0100

    fix __dirname is not defined

--- dapp/tests/unit/extractContentFromZipFile.test.ts
+++ dapp/tests/unit/extractContentFromZipFile.test.ts
@@ -3,16 +3,21 @@ import path from 'path';
 
 describe('extractContentFromZipFile', () => {
   it('should extract content from a valid .zip file', async () => {
-    const zipPath = path.join(__dirname, '../_test_inputs_/data.zip');
+    const zipPath = path.join(
+      process.cwd(),
+      '/dapp/tests/_test_inputs_/data.zip'
+    );
     const contentPath = '/content.txt';
-
     const result = await extractContentFromZipFile(zipPath, contentPath);
     const expectedContent = 'Test content';
     expect(result.toString()).toEqual(expectedContent);
   });
 
   it('should throw an error if no content file is found in the zip', async () => {
-    const zipPath = path.join(__dirname, '../_test_inputs_/data.zip');
+    const zipPath = path.join(
+      process.cwd(),
+      '/dapp/tests/_test_inputs_/data.zip'
+    );
     const nonExistentContentPath = '/non/existent/content';
 
     await expect(
@@ -21,7 +26,10 @@ describe('extractContentFromZipFile', () => {
   });
 
   it('should throw an error for a non-existent file', async () => {
-    const zipPath = path.join(__dirname, '../_test_inputs_/not_existent.zip');
+    const zipPath = path.join(
+      process.cwd(),
+      '/dapp/tests/_test_inputs_/not_existent.zip'
+    );
     const contentPath = '/content.txt';
 
     await expect(
