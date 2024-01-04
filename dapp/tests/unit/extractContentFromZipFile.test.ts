e810cf9a376272b282b40b0ffad1d2d73f7446a8
Abbes Benayache <benayache.abbes@gmail.com>
2024-01-04 17:07:29 +0100

    fix import.meta usage in tests

--- dapp/tests/unit/extractContentFromZipFile.test.ts
+++ dapp/tests/unit/extractContentFromZipFile.test.ts
@@ -1,35 +1,33 @@
 import extractContentFromZipFile from '../../src/extractContentFromZipFile';
+import path from 'path';
 
 describe('extractContentFromZipFile', () => {
   it('should extract content from a valid .zip file', async () => {
-    const zipPath = new URL('../_test_inputs_/data.zip', import.meta.url);
+    const zipPath = path.join(__dirname, '../_test_inputs_/data.zip');
     const contentPath = '/content.txt';
 
-    const result = await extractContentFromZipFile(
-      zipPath.pathname,
-      contentPath
-    );
+    const result = await extractContentFromZipFile(zipPath, contentPath);
     const expectedContent = 'Test content';
     expect(result.toString()).toEqual(expectedContent);
   });
 
   it('should throw an error if no content file is found in the zip', async () => {
-    const zipPath = new URL('../_test_inputs_/data.zip', import.meta.url);
+    const zipPath = path.join(__dirname, '../_test_inputs_/data.zip');
     const nonExistentContentPath = '/non/existent/content';
 
     await expect(
-      extractContentFromZipFile(zipPath.pathname, nonExistentContentPath)
+      extractContentFromZipFile(zipPath, nonExistentContentPath)
     ).rejects.toThrow('No content file was found in the zip.');
   });
 
   it('should throw an error for a non-existent file', async () => {
-    const zipPath = new URL('../_test_inputs_/test.zip', import.meta.url);
+    const zipPath = path.join(__dirname, '../_test_inputs_/not_existent.zip');
     const contentPath = '/content.txt';
 
     await expect(
-      extractContentFromZipFile(zipPath.pathname, contentPath)
+      extractContentFromZipFile(zipPath, contentPath)
     ).rejects.toThrow(
-      `Error: ENOENT: no such file or directory, open '${zipPath.pathname}'`
+      `Error: ENOENT: no such file or directory, open '${zipPath}'`
     );
   });
 });
