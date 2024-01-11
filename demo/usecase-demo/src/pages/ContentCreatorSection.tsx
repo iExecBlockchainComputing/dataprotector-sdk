18897d8e53631da217e36020d95aa1affb818612
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-01-11 15:21:54 +0100

    Keep coding Home

--- demo/usecase-demo/src/pages/ContentCreatorSection.tsx
+++ demo/usecase-demo/src/pages/ContentCreatorSection.tsx
@@ -1,9 +1,8 @@
-
-import { CheckCircle, DownloadCloud, Key } from 'react-feather';
+import { ArrowUpRight, CheckCircle, DownloadCloud, Key } from 'react-feather';
 import { Button } from '../components/ui/button.tsx';
 import { cn } from '../utils/style.utils.ts';
 
-export function ContentCreatorSection({className}: {className?: string}) {
+export function ContentCreatorSection({ className }: { className?: string }) {
   return (
     <div className={cn('flex gap-x-28', className)}>
       <div className="flex-1">
@@ -11,24 +10,34 @@ export function ContentCreatorSection({className}: {className?: string}) {
           Become Content Creator and share the content of your choice
         </h2>
         <p className="mt-[36px]">
-          Texte à ajouter - texte à ajouter - texte à ajouter - texte à ajouter<br />
-          Texte à ajouter - texte à ajouter - texte à ajouter - texte à ajouter<br />
-          Texte à ajouter - texte à ajouter - texte à ajouter - texte à ajouter<br />
+          Texte à ajouter - texte à ajouter - texte à ajouter - texte à ajouter
+          <br />
+          Texte à ajouter - texte à ajouter - texte à ajouter - texte à ajouter
+          <br />
+          Texte à ajouter - texte à ajouter - texte à ajouter - texte à ajouter
+          <br />
         </p>
         <div className="mt-[60px]">
-          <Button>Let's go</Button>
+          <Button>
+            Let's go
+            <ArrowUpRight size="20" className="-mr-1 ml-1.5" />
+          </Button>
         </div>
       </div>
-      <div className="flex-1 flex gap-y-6 flex-col">
-        <div className="rounded-3xl gap-x-6 border border-grey-700 px-11 py-10 flex">
+      <div className="flex flex-1 flex-col gap-y-6">
+        <div className="flex gap-x-6 rounded-3xl border border-grey-700 px-11 py-10">
           <CheckCircle className="shrink-0" size="32" />
-          <span className="text-lg">Choose your monetization (rent, subscription, sell)</span>
+          <span className="text-lg">
+            Choose your monetization (rent, subscription, sell)
+          </span>
         </div>
-        <div className="rounded-3xl gap-x-6 border border-grey-700 px-11 py-10 flex">
+        <div className="flex gap-x-6 rounded-3xl border border-grey-700 px-11 py-10">
           <DownloadCloud className="shrink-0" size="32" />
-          <span className="text-lg">Upload your content (pdf, articles, code, graphic assets)</span>
+          <span className="text-lg">
+            Upload your content (pdf, articles, code, graphic assets)
+          </span>
         </div>
-        <div className="rounded-3xl gap-x-6 border border-grey-700 px-11 py-10 flex">
+        <div className="flex gap-x-6 rounded-3xl border border-grey-700 px-11 py-10">
           <Key className="shrink-0" size="32" />
           <span className="text-lg">Ownership</span>
         </div>
