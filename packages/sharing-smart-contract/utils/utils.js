2f55fc6f0ab393427caee495b60f45212926324d
Le-Caignec <robinlecaignec@yahoo.fr>
2024-01-16 12:52:00 +0100

    save address & args into file

--- packages/sharing-smart-contract/utils/utils.js
+++ packages/sharing-smart-contract/utils/utils.js
@@ -1,10 +1,19 @@
 import fs from 'fs/promises';
-import { SMART_CONTRACT_ADDRESS_FILE } from '../config/config';
+
+// if you change the name of the file, you need to change the name in the pipeline too
+const SMART_CONTRACT_ADDRESS_FILE = '.smart-contract-address';
 
 /**
  * save the app address in `.smart-contract-address` file for next usages
  */
- const saveSmartContractAddress = (address) =>
+export const saveSmartContractAddress = (address) =>
   fs.writeFile(SMART_CONTRACT_ADDRESS_FILE, address);
 
-export default saveSmartContractAddress;
+// if you change the name of the file, you need to change the name in the pipeline too
+const CONSTRUCTOR_ARGS_PARAMS_FILE = '.constructor-args-params';
+
+/**
+ * save the app address in `.constructor-arg-params` file for next usages
+ */
+export const saveConstructorArgsParams = (address) =>
+  fs.writeFile(CONSTRUCTOR_ARGS_PARAMS_FILE, address);
