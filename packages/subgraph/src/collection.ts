e0a672e675589afafa54515f16039eee159dd2e9
Le-Caignec <robinlecaignec@yahoo.fr>
2024-01-18 19:07:02 +0100

    fix derivedFrom

--- packages/subgraph/src/collection.ts
+++ packages/subgraph/src/collection.ts
@@ -1,41 +1,38 @@
-import { Bytes } from "@graphprotocol/graph-ts";
 import {
-    Transfer as TransferEvent,
-    AddProtectedDataToCollection as AddProtectedDataToCollectionEvent,
-    RemoveProtectedDataFromCollection as RemoveProtectedDataFromCollectionEvent
-  } from "../generated/Collection/Collection"
-import { Collection } from "../generated/schema";
-import { intToAddress } from "./utils"
+  Transfer as TransferEvent,
+  AddProtectedDataToCollection as AddProtectedDataToCollectionEvent,
+  RemoveProtectedDataFromCollection as RemoveProtectedDataFromCollectionEvent,
+} from "../generated/Collection/Collection";
+import { Collection, ProtectedData } from "../generated/schema";
 
 export function handleTransfer(event: TransferEvent): void {
-  let collection = Collection.load(intToAddress(event.params.tokenId))
+  let collection = Collection.load(event.params.tokenId.toHex());
   if (!collection) {
-     collection = new Collection(intToAddress(event.params.tokenId))
-     collection.creationTimestamp = event.block.timestamp
-     collection.blockNumber = event.block.number
-     collection.transactionHash = event.transaction.hash
-     collection.protectedDatas = new Array<Bytes>()
+    collection = new Collection(event.params.tokenId.toHex());
+    collection.creationTimestamp = event.block.timestamp;
+    collection.blockNumber = event.block.number;
+    collection.transactionHash = event.transaction.hash;
   }
-  collection.owner = event.params.to
-  collection.save()
+  collection.owner = event.params.to;
+  collection.save();
 }
 
-export function handleAddProtectedDataToCollection(event: AddProtectedDataToCollectionEvent): void {
-  const collection = Collection.load(intToAddress(event.params.collectionId))
-  if (collection) {
-    const protectedData = event.params.protectedData.toHex()
-    collection.protectedDatas!.push(Bytes.fromHexString(protectedData))
-    collection.save()
+export function handleAddProtectedDataToCollection(
+  event: AddProtectedDataToCollectionEvent
+): void {
+  const protectedData = ProtectedData.load(event.params.protectedData);
+  if (protectedData) {
+    protectedData.collection = null;
+    protectedData.save();
   }
 }
 
-export function handleRemoveProtectedDataFromCollection(event: RemoveProtectedDataFromCollectionEvent): void {
-  const collection = Collection.load(intToAddress(event.params.collectionId));
-  if (collection) {
-    const index = collection.protectedDatas!.indexOf(Bytes.fromHexString(event.params.protectedData.toHex()));
-    if (index !== -1) {
-      collection.protectedDatas!.splice(index, 1);
-    }
-    collection.save()
+export function handleRemoveProtectedDataFromCollection(
+  event: RemoveProtectedDataFromCollectionEvent
+): void {
+  const protectedData = ProtectedData.load(event.params.protectedData);
+  if (protectedData) {
+    protectedData.collection = event.params.collectionId.toHex();
+    protectedData.save();
   }
 }
