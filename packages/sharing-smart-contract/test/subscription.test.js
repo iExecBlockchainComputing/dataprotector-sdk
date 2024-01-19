4ad74f812bf38d4b3d768a2b446fbaacf3dfccdd
Le-Caignec <robinlecaignec@yahoo.fr>
2024-01-19 10:56:24 +0100

    refactor tests

--- packages/sharing-smart-contract/test/subscription.test.js
+++ packages/sharing-smart-contract/test/subscription.test.js
@@ -25,32 +25,31 @@ describe('Subscription.sol', () => {
     return [params.price, params.duration];
   }
 
+  async function createCollection() {
+    const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
+    const tx = await subscriptionContract.connect(addr1).createCollection();
+    const receipt = await tx.wait();
+    const collectionTokenId = ethers.toNumber(receipt.logs[0].args[2]);
+    return { subscriptionContract, addr1, collectionTokenId };
+  }
+
   describe('subscribeTo()', () => {
     it('should allow a user to subscribe to a collection', async () => {
-      describe('subscribeTo()', () => {
-  let subscriptionContract;
-  let addr1;
-
-  beforeEach(async () => {
-    ({ subscriptionContract, addr1 } = await loadFixture(deploySCFixture));
-    const tx = await subscriptionContract.connect(addr1).createCollection();
-    await tx.wait();
-  });
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const tokenId = ethers.toNumber(receipt.logs[0].args[2]);
-
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
       const subscriptionPrice = ethers.parseEther('0.5');
       const subscriptionParams = {
         price: subscriptionPrice,
         duration: 15,
       };
-      await subscriptionContract.connect(addr1).setSubscriptionParams(tokenId, subscriptionParams);
+      await subscriptionContract
+        .connect(addr1)
+        .setSubscriptionParams(collectionTokenId, subscriptionParams);
 
       const subscriberBalanceBefore = await ethers.provider.getBalance(addr1.address);
       const subscriptionTx = await subscriptionContract
         .connect(addr1)
-        .subscribeTo(tokenId, { value: subscriptionPrice });
+        .subscribeTo(collectionTokenId, { value: subscriptionPrice });
       const subscriptionReceipt = await subscriptionTx.wait();
 
       const blockTimestamp = (await ethers.provider.getBlock(subscriptionReceipt.blockNumber))
@@ -62,7 +61,7 @@ describe('Subscription.sol', () => {
         .withArgs(addr1.address, expectedEndDate);
       const subscriptionInfo = await subscriptionContract
         .connect(addr1)
-        .subscribers(tokenId, addr1.address);
+        .subscribers(collectionTokenId, addr1.address);
       const subscriberBalanceAfter = await ethers.provider.getBalance(addr1.address);
 
       expect(ethers.toNumber(subscriptionInfo)).to.equal(expectedEndDate);
@@ -71,41 +70,38 @@ describe('Subscription.sol', () => {
     });
 
     it('should revert if subscription parameters are not set', async () => {
-      const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const tokenId = ethers.toNumber(receipt.logs[0].args[2]);
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
 
       await expect(
         subscriptionContract
           .connect(addr1)
-          .subscribeTo(tokenId, { value: ethers.parseEther('0.1') }),
+          .subscribeTo(collectionTokenId, { value: ethers.parseEther('0.1') }),
       ).to.be.revertedWith('Subscription parameters not set');
     });
 
     it('should revert if the subscription price is lower than required', async () => {
-      const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const tokenId = ethers.toNumber(receipt.logs[0].args[2]);
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
+
       const subscriptionParams = {
         price: ethers.parseEther('1'),
         duration: 30,
       };
-      await subscriptionContract.connect(addr1).setSubscriptionParams(tokenId, subscriptionParams);
+      await subscriptionContract
+        .connect(addr1)
+        .setSubscriptionParams(collectionTokenId, subscriptionParams);
 
       await expect(
         subscriptionContract
           .connect(addr1)
-          .subscribeTo(tokenId, { value: ethers.parseEther('0.5') }),
+          .subscribeTo(collectionTokenId, { value: ethers.parseEther('0.5') }),
       ).to.be.revertedWith('Wrong amount sent');
     });
 
     it('should revert if the subscription price is not equal to value sent', async () => {
-      const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const tokenId = ethers.toNumber(receipt.logs[0].args[2]);
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
 
       const subscriptionPrice = ethers.parseEther('0.5');
       const tokenSended = ethers.parseEther('1.3'); // Send more money than required
@@ -115,13 +111,13 @@ describe('Subscription.sol', () => {
       };
       const SubscriptionOptionsTx = await subscriptionContract
         .connect(addr1)
-        .setSubscriptionParams(tokenId, subscriptionParams);
+        .setSubscriptionParams(collectionTokenId, subscriptionParams);
       await SubscriptionOptionsTx.wait();
 
       const subscriberBalanceBefore = await ethers.provider.getBalance(addr1.address);
       const subscriptionTx = subscriptionContract
         .connect(addr1)
-        .subscribeTo(tokenId, { value: tokenSended });
+        .subscribeTo(collectionTokenId, { value: tokenSended });
       await expect(subscriptionTx).to.be.revertedWith('Wrong amount sent');
       const subscriberBalanceAfter = await ethers.provider.getBalance(addr1.address);
 
@@ -132,32 +128,34 @@ describe('Subscription.sol', () => {
 
   describe('setSubscriptionParams()', () => {
     it('should set and get subscription parameters', async () => {
-      const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const tokenId = ethers.toNumber(receipt.logs[0].args[2]);
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
+
       const subscriptionParams = {
         price: ethers.parseEther('0.000001'),
         duration: 30,
       };
-      await subscriptionContract.connect(addr1).setSubscriptionParams(tokenId, subscriptionParams);
-      const retrievedParamsArray = await subscriptionContract.subscriptionParams(tokenId);
+      await subscriptionContract
+        .connect(addr1)
+        .setSubscriptionParams(collectionTokenId, subscriptionParams);
+      const retrievedParamsArray = await subscriptionContract.subscriptionParams(collectionTokenId);
 
       expect(subscriptionParamsToArray(subscriptionParams)).to.deep.equal(retrievedParamsArray);
     });
 
     it('should emit NewSubscriptionParams event', async () => {
-      const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const tokenId = ethers.toNumber(receipt.logs[0].args[2]);
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
+
       const subscriptionParams = {
         price: ethers.parseEther('0.5'),
         duration: 15,
       };
 
       await expect(
-        subscriptionContract.connect(addr1).setSubscriptionParams(tokenId, subscriptionParams),
+        subscriptionContract
+          .connect(addr1)
+          .setSubscriptionParams(collectionTokenId, subscriptionParams),
       )
         .to.emit(subscriptionContract, 'NewSubscriptionParams')
         .withArgs(subscriptionParamsToArray(subscriptionParams));
@@ -166,10 +164,8 @@ describe('Subscription.sol', () => {
 
   describe('setProtectedDataToSubscription()', () => {
     it('should set protected data to subscription', async () => {
-      const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const collectionTokenId = ethers.toNumber(receipt.logs[0].args[2]);
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
 
       const protectedDataAddress = await createDatasetForContract(addr1.address, rpcURL);
       const registry = await ethers.getContractAt(
@@ -200,18 +196,15 @@ describe('Subscription.sol', () => {
     });
 
     it('should revert if trying to set protectedData not own by the collection contract', async () => {
-      const { subscriptionContract, addr1 } = await loadFixture(deploySCFixture);
-      const tx = await subscriptionContract.connect(addr1).createCollection();
-      const receipt = await tx.wait();
-      const collectionTokenId = ethers.toNumber(receipt.logs[0].args[2]);
-
+      const { subscriptionContract, addr1, collectionTokenId } =
+        await loadFixture(createCollection);
       const protectedDataAddress = await createDatasetForContract(addr1.address, rpcURL);
 
       await expect(
         subscriptionContract
           .connect(addr1)
           .setProtectedDataToSubscription(collectionTokenId, protectedDataAddress),
-      ).to.be.revertedWith("ProtectedData is not in collection");
+      ).to.be.revertedWith('ProtectedData is not in collection');
     });
   });
 });
