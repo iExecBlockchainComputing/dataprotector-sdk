a005ff24c9b7816fd53e74421c40b5999e78fb04
Le-Caignec <robinlecaignec@yahoo.fr>
2024-01-22 22:38:11 +0100

    refactor subscription tests

--- packages/sharing-smart-contract/test/subscription.test.js
+++ packages/sharing-smart-contract/test/subscription.test.js
@@ -40,6 +40,46 @@ describe('Subscription.sol', () => {
     return { protectedDataSharingContract, collectionContract, collectionTokenId, addr1, addr2 };
   }
 
+  async function setProtectedDataToSubscription() {
+    const { protectedDataSharingContract, collectionContract, collectionTokenId, addr1, addr2 } =
+      await loadFixture(createCollection);
+
+    const subscriptionPrice = ethers.parseEther('0.5');
+    const subscriptionParams = {
+      price: subscriptionPrice,
+      duration: 15,
+    };
+    await protectedDataSharingContract
+      .connect(addr1)
+      .setSubscriptionParams(collectionTokenId, subscriptionParams);
+
+    const protectedDataAddress = await createDatasetForContract(addr1.address, rpcURL);
+    const registry = await ethers.getContractAt(
+      'IDatasetRegistry',
+      '0x799daa22654128d0c64d5b79eac9283008158730',
+    );
+    const protectedDataTokenId = ethers.getBigInt(protectedDataAddress.toLowerCase()).toString();
+    await registry
+      .connect(addr1)
+      .approve(await collectionContract.getAddress(), protectedDataTokenId);
+    await collectionContract
+      .connect(addr1)
+      .addProtectedDataToCollection(collectionTokenId, protectedDataAddress);
+    const setProtectedDataToSubscriptionTx = await protectedDataSharingContract
+      .connect(addr1)
+      .setProtectedDataToSubscription(collectionTokenId, protectedDataAddress);
+    const setProtectedDataToSubscriptionReceipt = await setProtectedDataToSubscriptionTx.wait();
+    return {
+      protectedDataSharingContract,
+      protectedDataAddress,
+      setProtectedDataToSubscriptionReceipt,
+      collectionTokenId,
+      subscriptionPrice,
+      addr1,
+      addr2,
+    };
+  }
+
   describe('subscribeTo()', () => {
     it('should allow a user to subscribe to a collection', async () => {
       const { protectedDataSharingContract, addr1, collectionTokenId } =
@@ -199,25 +239,12 @@ describe('Subscription.sol', () => {
 
   describe('setProtectedDataToSubscription()', () => {
     it('should set protected data to subscription', async () => {
-      const { protectedDataSharingContract, collectionContract, addr1, collectionTokenId } =
-        await loadFixture(createCollection);
-
-      const protectedDataAddress = await createDatasetForContract(addr1.address, rpcURL);
-      const registry = await ethers.getContractAt(
-        'IDatasetRegistry',
-        '0x799daa22654128d0c64d5b79eac9283008158730',
-      );
-      const protectedDataTokenId = ethers.getBigInt(protectedDataAddress.toLowerCase()).toString();
-      await registry
-        .connect(addr1)
-        .approve(await collectionContract.getAddress(), protectedDataTokenId);
-      await collectionContract
-        .connect(addr1)
-        .addProtectedDataToCollection(collectionTokenId, protectedDataAddress);
-      const setProtectedDataToSubscriptionTx = await protectedDataSharingContract
-        .connect(addr1)
-        .setProtectedDataToSubscription(collectionTokenId, protectedDataAddress);
-      const setProtectedDataToSubscriptionReceipt = await setProtectedDataToSubscriptionTx.wait();
+      const {
+        protectedDataSharingContract,
+        protectedDataAddress,
+        collectionTokenId,
+        setProtectedDataToSubscriptionReceipt,
+      } = await loadFixture(setProtectedDataToSubscription);
 
       expect(setProtectedDataToSubscriptionReceipt)
         .to.emit(protectedDataSharingContract, 'AddProtectedDataForSubscription')
@@ -245,24 +272,9 @@ describe('Subscription.sol', () => {
 
   describe('removeProtectedDataFromSubscription()', () => {
     it('should remove protected data from subscription', async () => {
-      const { protectedDataSharingContract, collectionContract, addr1, collectionTokenId } =
-        await loadFixture(createCollection);
+      const { protectedDataSharingContract, protectedDataAddress, collectionTokenId, addr1 } =
+        await loadFixture(setProtectedDataToSubscription);
 
-      const protectedDataAddress = await createDatasetForContract(addr1.address, rpcURL);
-      const registry = await ethers.getContractAt(
-        'IDatasetRegistry',
-        '0x799daa22654128d0c64d5b79eac9283008158730',
-      );
-      const protectedDataTokenId = ethers.getBigInt(protectedDataAddress.toLowerCase()).toString();
-      await registry
-        .connect(addr1)
-        .approve(await collectionContract.getAddress(), protectedDataTokenId);
-      await collectionContract
-        .connect(addr1)
-        .addProtectedDataToCollection(collectionTokenId, protectedDataAddress);
-      await protectedDataSharingContract
-        .connect(addr1)
-        .setProtectedDataToSubscription(collectionTokenId, protectedDataAddress);
       const removeProtectedDataToSubscriptionTx = await protectedDataSharingContract
         .connect(addr1)
         .removeProtectedDataFromSubscription(collectionTokenId, protectedDataAddress);
@@ -293,32 +305,15 @@ describe('Subscription.sol', () => {
     });
 
     it('should revert if trying to remove protectedData not own by the collection contract', async () => {
-      const { protectedDataSharingContract, collectionContract, collectionTokenId, addr1, addr2 } =
-        await loadFixture(createCollection);
-      const subscriptionPrice = ethers.parseEther('0.5');
-      const subscriptionParams = {
-        price: subscriptionPrice,
-        duration: 15,
-      };
-      await protectedDataSharingContract
-        .connect(addr1)
-        .setSubscriptionParams(collectionTokenId, subscriptionParams);
+      const {
+        protectedDataSharingContract,
+        protectedDataAddress,
+        collectionTokenId,
+        subscriptionPrice,
+        addr1,
+        addr2,
+      } = await loadFixture(setProtectedDataToSubscription);
 
-      const protectedDataAddress = await createDatasetForContract(addr1.address, rpcURL);
-      const registry = await ethers.getContractAt(
-        'IDatasetRegistry',
-        '0x799daa22654128d0c64d5b79eac9283008158730',
-      );
-      const protectedDataTokenId = ethers.getBigInt(protectedDataAddress.toLowerCase()).toString();
-      await registry
-        .connect(addr1)
-        .approve(await collectionContract.getAddress(), protectedDataTokenId);
-      await collectionContract
-        .connect(addr1)
-        .addProtectedDataToCollection(collectionTokenId, protectedDataAddress);
-      await protectedDataSharingContract
-        .connect(addr1)
-        .setProtectedDataToSubscription(collectionTokenId, protectedDataAddress);
       await protectedDataSharingContract
         .connect(addr2)
         .subscribeTo(collectionTokenId, { value: subscriptionPrice });
