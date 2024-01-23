52ff61cf5432d2a783ca8dd0be286e42d1d9ab2a
luisccastillo <luiscarloscastillo@protonmail.com>
2024-01-23 18:26:50 +0100

    update collection and subscriptions schema

--- packages/subgraph/src/collectionSubscription.ts
+++ packages/subgraph/src/collectionSubscription.ts
@@ -1,17 +1,16 @@
-
 import {
     NewSubscription as NewSubscriptionEvent,
     NewSubscriptionParams as NewSubscriptionParamsEvent,
     AddProtectedDataForSubscription as AddProtectedDataForSubscriptionEvent,
-  } from "../generated/CollectionSubscription/CollectionSubscription";
-import { SubscriptionsParams, CollectionSubscription } from "../generated/schema";
+  } from "../generated/ProtectedDataSharing/ProtectedDataSharing";
+import { SubscriptionParams, ProtectedData, CollectionSubscription } from "../generated/schema";
   
   export function handleNewSubscription(event: NewSubscriptionEvent): void {
     const subscription = new CollectionSubscription(
         event.params._collectionId.toHex()
     );
     subscription.collection = event.params._collectionId.toHex();
-    subscription.owner = event.params.subscriber.toHex();
+    subscription.subscriber = event.params.subscriber.toHex();
     subscription.endDate = event.params.endDate;
     subscription.blockNumber = event.block.number;
     subscription.creationTimestamp = event.block.timestamp;
@@ -22,8 +21,8 @@ import { SubscriptionsParams, CollectionSubscription } from "../generated/schema
   export function handleNewSubscriptionParams(
     event: NewSubscriptionParamsEvent
   ): void {
-    const subscriptionParams = new SubscriptionsParams(
-        event.transaction.hash.concatI32(event.logIndex.toI32())
+    const subscriptionParams = new SubscriptionParams(
+        event.params._collectionId.toHex()
     );
     subscriptionParams.duration = event.params.subscriptionParams.duration;
     subscriptionParams.price = event.params.subscriptionParams.price;
@@ -32,12 +31,9 @@ import { SubscriptionsParams, CollectionSubscription } from "../generated/schema
   export function handleAddProtectedDataForSubscription(
     event: AddProtectedDataForSubscriptionEvent
   ): void {
-    const subscription = CollectionSubscription.load(
-      event.params._collectionId.toHex()
-    );
-    if (subscription) {
-      subscription.protectedData = event.params._protectedData;
-      subscription.save();
+    const protectedData = ProtectedData.load(event.params._protectedData);
+    if (protectedData) {
+      protectedData.isIncludedInSubscription = true;
+      protectedData.save();
     }
   }
-  
