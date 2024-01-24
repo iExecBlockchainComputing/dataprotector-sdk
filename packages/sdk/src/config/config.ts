3b6ec891b6fc8fb915d5e64850d7371f2a394ebe
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-01-24 17:38:54 +0100

    Add PoCo registry SC address + update sharing SC address + update subgraph URL

--- packages/sdk/src/config/config.ts
+++ packages/sdk/src/config/config.ts
@@ -1,20 +1,36 @@
 export const DEFAULT_IPFS_GATEWAY = 'https://ipfs-gateway.v8-bellecour.iex.ec';
+
 export const DEFAULT_IEXEC_IPFS_NODE =
   'https://ipfs-upload.v8-bellecour.iex.ec';
+
 export const DEFAULT_DATA_NAME = '';
+
 export const NETWORK = {
   chainId: 134,
   name: 'iExec Sidechain',
   url: 'https://bellecour.iex.ec',
 };
+
+// Contract Name: DataProtector
 export const DEFAULT_CONTRACT_ADDRESS =
   '0x3a4Ab33F3D605e75b6D00A32A0Fa55C3628F6A59';
+
+// iExec Dataset Registry
+export const POCO_REGISTRY_CONTRACT_ADDRESS =
+  '0x799daa22654128d0c64d5b79eac9283008158730';
+
 export const DEFAULT_SHARING_CONTRACT_ADDRESS =
-  '0x1113284fa580806C11B85083087e358F5f4746C8';
+  '0xA73A3518F629834630ef98E57C573018eA198365';
+
 export const DEFAULT_SUBGRAPH_URL =
-  'https://thegraph-product.iex.ec/subgraphs/name/bellecour/dataprotector';
+  'https://thegraph-product.iex.ec/subgraphs/name/bellecour/dev-dataprotector-v2';
+
 export const WORKERPOOL_ADDRESS = 'prod-v8-bellecour.main.pools.iexec.eth';
+
 export const SCONE_TAG = ['tee', 'scone'];
+
 export const DEFAULT_MAX_PRICE = 0;
+
 export const ADD_RESOURCE_SELECTOR = 'addResourceToWhitelist(address)';
+
 export const KEY_PURPOSE_SELECTOR = 'keyHasPurpose(bytes32,uint256)';
