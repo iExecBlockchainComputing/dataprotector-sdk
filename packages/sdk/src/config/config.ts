12725eb89f65f707d4c70050b25a7fbb90b3c750
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-01-30 18:27:05 +0100

    Use dev v2 subgraph URL + update DEFAULT_SHARING_CONTRACT_ADDRESS

--- packages/sdk/src/config/config.ts
+++ packages/sdk/src/config/config.ts
@@ -1,20 +1,32 @@
 export const DEFAULT_IPFS_GATEWAY = 'https://ipfs-gateway.v8-bellecour.iex.ec';
+
 export const DEFAULT_IEXEC_IPFS_NODE =
   'https://ipfs-upload.v8-bellecour.iex.ec';
+
 export const DEFAULT_DATA_NAME = '';
+
 export const NETWORK = {
   chainId: 134,
   name: 'iExec Sidechain',
   url: 'https://bellecour.iex.ec',
 };
+
+// Original DataProtector smart-contract
 export const DEFAULT_CONTRACT_ADDRESS =
   '0x3a4Ab33F3D605e75b6D00A32A0Fa55C3628F6A59';
+
 export const DEFAULT_SHARING_CONTRACT_ADDRESS =
-  '0x1113284fa580806C11B85083087e358F5f4746C8';
+  '0xC7bdd37e598C03e585800b2644807442efa9736D';
+
 export const DEFAULT_SUBGRAPH_URL =
-  'https://thegraph-product.iex.ec/subgraphs/name/bellecour/dataprotector';
+  'https://thegraph-product.iex.ec/subgraphs/name/bellecour/dev-dataprotector-v2';
+
 export const WORKERPOOL_ADDRESS = 'prod-v8-bellecour.main.pools.iexec.eth';
+
 export const SCONE_TAG = ['tee', 'scone'];
+
 export const DEFAULT_MAX_PRICE = 0;
+
 export const ADD_RESOURCE_SELECTOR = 'addResourceToWhitelist(address)';
+
 export const KEY_PURPOSE_SELECTOR = 'keyHasPurpose(bytes32,uint256)';
