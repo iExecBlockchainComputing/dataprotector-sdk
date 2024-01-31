339155b59a74c69702f11e2366ebae423cdeb4a9
SeddikBellamine <bellamine.seddik@gmail.com>
2024-01-31 03:59:23 +0100

    fix createCollection method after smart contract changes

--- packages/sdk/src/dataProtector/sharing/createCollection.ts
+++ packages/sdk/src/dataProtector/sharing/createCollection.ts
@@ -1,24 +1,24 @@
 import { ethers, type Contract } from 'ethers';
-import { ABI as sharingABI } from '../../contracts/sharingAbi.js';
+import { ABI as collectionABI } from '../../contracts/collectionAbi.js';
 import { WorkflowError } from '../../utils/errors.js';
 import { throwIfMissing } from '../../utils/validators.js';
 import {
   AddressOrENS,
-  IExecConsumer,
   CreateCollectionResponse,
+  IExecConsumer,
 } from '../types.js';
 
 export const createCollection = async ({
   iexec = throwIfMissing(),
-  sharingContractAddress,
+  collectionContractAddress,
 }: IExecConsumer & {
-  sharingContractAddress: AddressOrENS;
+  collectionContractAddress: AddressOrENS;
 }): Promise<CreateCollectionResponse> => {
   const { provider, signer } = await iexec.config.resolveContractsClient();
 
   const sharingContract = new ethers.Contract(
-    sharingContractAddress,
-    sharingABI,
+    collectionContractAddress,
+    collectionABI,
     provider
   );
 
