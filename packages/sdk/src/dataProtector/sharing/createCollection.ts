97a744564cd3b72cb3863db46844e595cd7a389d
SeddikBellamine <bellamine.seddik@gmail.com>
2024-01-31 16:12:55 +0100

    fix contract naming in createCollection

--- packages/sdk/src/dataProtector/sharing/createCollection.ts
+++ packages/sdk/src/dataProtector/sharing/createCollection.ts
@@ -16,18 +16,20 @@ export const createCollection = async ({
 }): Promise<CreateCollectionResponse> => {
   const { provider, signer } = await iexec.config.resolveContractsClient();
 
-  const sharingContract = new ethers.Contract(
+  const collectionContract = new ethers.Contract(
     collectionContractAddress,
     collectionABI,
     provider
   );
 
-  const transactionReceipt = await (sharingContract.connect(signer) as Contract)
+  const transactionReceipt = await (
+    collectionContract.connect(signer) as Contract
+  )
     .createCollection()
     .then((tx) => tx.wait())
     .catch((e: Error) => {
       throw new WorkflowError(
-        'Failed to create collection into sharing smart contract',
+        'Failed to create collection into collection smart contract',
         e
       );
     });
