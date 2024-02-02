76edf642c1d42d9315dd8ec35b0385537c30e578
luisccastillo <luiscarloscastillo@protonmail.com>
2024-02-02 18:58:21 +0100

    index renting events

--- packages/subgraph/src/datasetRegistry.ts
+++ packages/subgraph/src/datasetRegistry.ts
@@ -1,8 +1,8 @@
-import { Bytes, BigInt } from "@graphprotocol/graph-ts";
-import { Dataset as DatasetContract } from "../generated/DatasetRegistry/Dataset";
-import { Transfer as TransferEvent } from "../generated/DatasetRegistry/DatasetRegistry";
-import { Account, ProtectedData } from "../generated/schema";
-import { intToAddress } from "./utils";
+import { Bytes, BigInt } from '@graphprotocol/graph-ts';
+import { Dataset as DatasetContract } from '../generated/DatasetRegistry/Dataset';
+import { Transfer as TransferEvent } from '../generated/DatasetRegistry/DatasetRegistry';
+import { Account, ProtectedData } from '../generated/schema';
+import { intToAddress } from './utils';
 
 export function handleTransferDataset(ev: TransferEvent): void {
   let contract = DatasetContract.bind(intToAddress(ev.params.tokenId));
@@ -23,15 +23,16 @@ export function handleTransferDataset(ev: TransferEvent): void {
     protectedData.creationTimestamp = ev.block.timestamp;
 
     // Will be filled by the DatasetSchemaEvent.
-    protectedData.jsonSchema = "";
+    protectedData.jsonSchema = '';
     protectedData.schema = new Array<string>();
-    protectedData.transactionHash = Bytes.fromHexString("0x");
+    protectedData.transactionHash = Bytes.fromHexString('0x');
     protectedData.blockNumber = BigInt.fromI32(0);
   }
 
   protectedData.owner = contract.owner().toHex();
   protectedData.name = contract.m_datasetName();
   protectedData.isIncludedInSubscription = false;
+  protectedData.isRentable = false;
   protectedData.multiaddr = contract.m_datasetMultiaddr();
   protectedData.checksum = contract.m_datasetChecksum();
 
---
+++ packages/subgraph/src/protectedDataSharing.ts
@@ -0,0 +1,160 @@
+import {
+  NewSubscription as NewSubscriptionEvent,
+  NewSubscriptionParams as NewSubscriptionParamsEvent,
+  ProtectedDataAddedForSubscription as ProtectedDataAddedForSubscriptionEvent,
+  ProtectedDataRemovedFromSubscription as ProtectedDataRemovedFromSubscriptionEvent,
+  Transfer as TransferEvent,
+  ProtectedDataAddedToCollection as ProtectedDataAddedToCollectionEvent,
+  ProtectedDataRemovedFromCollection as ProtectedDataRemovedFromCollectionEvent,
+  NewRental as NewRentalEvent,
+  ProtectedDataAddedForRenting as ProtectedDataAddedForRentingEvent,
+  ProtectedDataRemovedFromRenting as ProtectedDataRemovedFromRentingEvent,
+} from '../generated/ProtectedDataSharing/ProtectedDataSharing';
+import {
+  SubscriptionParam,
+  ProtectedData,
+  CollectionSubscription,
+  Collection,
+  Rental,
+  RentalParam,
+} from '../generated/schema';
+
+export function handleTransfer(event: TransferEvent): void {
+  let collection = Collection.load(event.params.tokenId.toHex());
+  if (!collection) {
+    collection = new Collection(event.params.tokenId.toHex());
+    collection.creationTimestamp = event.block.timestamp;
+    collection.blockNumber = event.block.number;
+    collection.transactionHash = event.transaction.hash;
+  }
+  collection.owner = event.params.to.toHex();
+  collection.save();
+}
+
+export function handleProtectedDataAddedToCollection(
+  event: ProtectedDataAddedToCollectionEvent
+): void {
+  const protectedData = ProtectedData.load(event.params.protectedData);
+  if (protectedData) {
+    protectedData.collection = event.params.collectionId.toHex();
+    protectedData.save();
+  }
+}
+
+export function handleProtectedDataRemovedFromCollection(
+  event: ProtectedDataRemovedFromCollectionEvent
+): void {
+  const protectedData = ProtectedData.load(event.params.protectedData);
+  if (protectedData) {
+    protectedData.collection = null;
+    protectedData.save();
+  }
+}
+
+export function handleNewSubscription(event: NewSubscriptionEvent): void {
+  const subscription = new CollectionSubscription(
+    event.params._collectionId.toHex()
+  );
+  subscription.collection = event.params._collectionId.toHex();
+  subscription.subscriber = event.params.subscriber.toHex();
+  subscription.endDate = event.params.endDate;
+  subscription.blockNumber = event.block.number;
+  subscription.creationTimestamp = event.block.timestamp;
+  subscription.transactionHash = event.transaction.hash;
+  subscription.save();
+}
+
+export function handleNewSubscriptionParams(
+  event: NewSubscriptionParamsEvent
+): void {
+  let subscriptionParams = SubscriptionParam.load(
+    event.params._collectionId.toHex()
+  );
+  const collection = Collection.load(event.params._collectionId.toHex());
+  if (!subscriptionParams) {
+    subscriptionParams = new SubscriptionParam(
+      event.params._collectionId.toHex()
+    );
+  }
+  subscriptionParams.duration = event.params.subscriptionParams.duration;
+  subscriptionParams.price = event.params.subscriptionParams.price;
+
+  if (collection) {
+    collection.subscriptionParams = subscriptionParams.id;
+    collection.save();
+  }
+  subscriptionParams.save();
+}
+
+export function handleProtectedDataAddedForSubscription(
+  event: ProtectedDataAddedForSubscriptionEvent
+): void {
+  const protectedData = ProtectedData.load(event.params._protectedData);
+  if (protectedData) {
+    protectedData.isIncludedInSubscription = true;
+    protectedData.save();
+  }
+}
+
+export function handleProtectedDataRemovedFromSubscription(
+  event: ProtectedDataRemovedFromSubscriptionEvent
+): void {
+  const protectedData = ProtectedData.load(event.params._protectedData);
+  if (protectedData) {
+    protectedData.isIncludedInSubscription = false;
+    protectedData.save();
+  }
+}
+
+export function handleNewRental(event: NewRentalEvent): void {
+  const rental = new Rental(
+    (
+      event.params._protectedData.toI64() +
+      event.params.renter.toI64() +
+      event.block.timestamp.toI64() +
+      event.logIndex.toI64()
+    ).toString()
+  );
+  const protectedData = ProtectedData.load(event.params._protectedData);
+  if (protectedData) {
+    rental.protectedData = protectedData.id;
+    const rentalParam = RentalParam.load(protectedData.id.toHex());
+    if (rentalParam) {
+      rental.rentalParams = rentalParam.id;
+    }
+  }
+  const collection = Collection.load(event.params._collectionId.toHex());
+  if (collection) {
+    rental.collection = collection.id;
+  }
+  rental.creationTimestamp  = event.block.timestamp;
+  rental.endDate = event.params.endDate;
+  rental.renter = event.params.renter;
+  rental.blockNumber = event.block.number;
+  rental.transactionHash = event.transaction.hash;
+  rental.save();
+}
+
+export function handleProtectedDataAddedForRenting(
+  event: ProtectedDataAddedForRentingEvent
+): void {
+  const protectedData = ProtectedData.load(event.params._protectedData);
+  if (protectedData) {
+    protectedData.isRentable = true;
+    const rentalParam = new RentalParam(protectedData.id.toHex());
+    rentalParam.duration = event.params._duration;
+    rentalParam.price = event.params._price;
+    rentalParam.save();
+    protectedData.save();
+  }
+}
+
+export function handleProtectedDataRemovedFromRenting(
+  event: ProtectedDataRemovedFromRentingEvent
+): void {
+  const protectedData = ProtectedData.load(event.params._protectedData);
+  if (protectedData) {
+    protectedData.isRentable = false;
+    protectedData.save();
+  }
+}
