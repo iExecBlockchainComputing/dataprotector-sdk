0323e9d8b32af44bba47f8aef3513386fa35a273
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-02-06 17:36:53 +0100

    Prepare to subscribe to user

--- packages/demo/usecase-demo/src/routes/user.$userId.tsx
+++ packages/demo/usecase-demo/src/routes/user.$userId.tsx
@@ -1,10 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 import { clsx } from 'clsx';
 import { useEffect, useState } from 'react';
+import { Loader } from 'react-feather';
+import { Alert } from '../components/Alert.tsx';
+import { Button } from '../components/ui/button.tsx';
 import { getDataProtectorClient } from '../externals/dataProtectorClient.ts';
 import { getEnsForAddress } from '../externals/getEnsForAddress.ts';
 import { useUserStore } from '../stores/user.store.ts';
+import { secondsToDays } from '../utils/secondsToDays.ts';
 import { timestampToReadableDate } from '../utils/timestampToReadableDate.ts';
 import styles from './_profile.module.css';
 
@@ -14,9 +18,11 @@ export const Route = createFileRoute('/user/$userId')({
 
 export function UserProfile() {
   const { userId } = Route.useParams();
-  const [ensName, setEnsName] = useState();
 
   const { isConnected } = useUserStore();
+  const queryClient = useQueryClient();
+
+  const [ensName, setEnsName] = useState();
 
   useEffect(() => {
     function getEns() {
@@ -36,6 +42,21 @@ export function UserProfile() {
       });
     },
     enabled: isConnected,
+  });
+
+  const subscribeMutation = useMutation({
+    mutationFn: async () => {
+      if (!collections?.[0]) {
+        return;
+      }
+      const dataProtector = await getDataProtectorClient();
+      return dataProtector.subscribe({
+        collectionId: Number(collections[0]),
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['collections', userId] });
+    },
   });
 
   return (
@@ -60,17 +81,57 @@ export function UserProfile() {
       )}
 
       {collections?.[0] && (
-        <div className="mt-3">
-          <div>Collection {Number(collections[0].id)}</div>
-          <div>
-            Created: {timestampToReadableDate(collections[0].creationTimestamp)}
+        <>
+          <div className="mt-3">
+            <div>Collection no: {Number(collections[0].id)}</div>
+            <div>
+              Created:{' '}
+              {timestampToReadableDate(collections[0].creationTimestamp)}
+            </div>
+            <div>{collections[0].protectedDatas?.length} protected data</div>
+            <div>{collections[0].subscriptions?.length} subscribers</div>
+            <div>Price in nRLC: {collections[0].subscriptionParams?.price}</div>
+            <div>
+              Duration in days:{' '}
+              {secondsToDays(
+                Number(collections[0].subscriptionParams?.duration)
+              )}
+            </div>
           </div>
-          <div>{collections[0].protectedDatas?.length} protected data</div>
-          <div>{collections[0].subscriptions?.length} subscribers</div>
-          <div>Price in nRLC: {collections[0].subscriptionParams?.price}</div>
-          <div>
-            Duration in seconds: {collections[0].subscriptionParams?.duration}
+
+          <div className="mt-6">
+            <Button
+              size="sm"
+              disabled={subscribeMutation.isPending}
+              className="btn btn-primary"
+              onClick={() => {
+                subscribeMutation.mutate();
+              }}
+            >
+              {subscribeMutation.isPending && (
+                <Loader size="16" className="mr-2 animate-spin-slow" />
+              )}
+              <span>Subscribe</span>
+            </Button>
+
+            {subscribeMutation.isError && (
+              <Alert variant="error" className="mt-4">
+                {subscribeMutation.error.toString()}
+              </Alert>
+            )}
           </div>
+        </>
+      )}
+
+      {collections?.[1] && (
+        <div className="mt-4 italic">
+          User has other collections that are not displayed in this
+          usecase-demo. (
+          {collections
+            .slice(1)
+            .map((c) => Number(c.id))
+            .join(', ')}
+          )
         </div>
       )}
     </div>
