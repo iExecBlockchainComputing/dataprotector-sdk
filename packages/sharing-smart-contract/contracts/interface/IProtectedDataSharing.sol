b53a7c4dc3dfc937dda72dbfcb859697683c1d88
Le-Caignec <robinlecaignec@yahoo.fr>
2024-02-12 13:58:36 +0100

    refactor collectionId to collectionTokenId

--- packages/sharing-smart-contract/contracts/interface/IProtectedDataSharing.sol
+++ packages/sharing-smart-contract/contracts/interface/IProtectedDataSharing.sol
@@ -41,14 +41,14 @@ interface IProtectedDataSharing is ICollection, ISubscription, IRental, ISale {
     /**
      * Consume protected data by creating a deal on the iExec platform.
      * Requires a valid subscription or rental for the protected data.
-     * @param _collectionId The ID of the collection containing the protected data.
+     * @param _collectionTokenId The ID of the collection containing the protected data.
      * @param _protectedData The address of the protected data.
      * @param _workerpoolOrder The workerpool order for the computation task.
      * @param _contentPath The path of the content inside the protected data to consume.
      * @return The unique identifier (deal ID) of the created deal on the iExec platform.
      */
     function consumeProtectedData(
-        uint256 _collectionId,
+        uint256 _collectionTokenId,
         address _protectedData,
         IexecLibOrders_v5.WorkerpoolOrder calldata _workerpoolOrder,
         string calldata _contentPath
