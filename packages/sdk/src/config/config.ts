1d0fa5d53f49427d5c5509d244370f24d62ec46b
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-02-14 17:20:04 +0100

    Finally set all these addresses to lowerCase!

--- packages/sdk/src/config/config.ts
+++ packages/sdk/src/config/config.ts
@@ -13,17 +13,17 @@ export const NETWORK = {
 
 // Original DataProtector smart-contract
 export const DEFAULT_CONTRACT_ADDRESS =
-  '0x3a4Ab33F3D605e75b6D00A32A0Fa55C3628F6A59';
+  '0x3a4Ab33F3D605e75b6D00A32A0Fa55C3628F6A59'.toLowerCase();
 
 // iExec Dataset Registry
 export const POCO_REGISTRY_CONTRACT_ADDRESS =
-  '0x799daa22654128d0c64d5b79eac9283008158730';
+  '0x799daa22654128d0c64d5b79eac9283008158730'.toLowerCase();
 
 export const DEFAULT_SHARING_CONTRACT_ADDRESS =
-  '0x721a0d237a40Ff5Ea40Ad5eb0c8e5101d5aa5856';
+  '0x721a0d237a40Ff5Ea40Ad5eb0c8e5101d5aa5856'.toLowerCase();
 
 export const DEFAULT_PROTECTED_DATA_SHARING_APP =
-  '0x4369191f463c48c2cd8D013Cf7CbE2DECeDB054b';
+  '0x4369191f463c48c2cd8D013Cf7CbE2DECeDB054b'.toLowerCase();
 
 export const DEFAULT_SUBGRAPH_URL =
   'https://thegraph-product.iex.ec/subgraphs/name/bellecour/dev-dataprotector-v2';
