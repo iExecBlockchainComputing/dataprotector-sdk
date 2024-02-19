6e5837ce02ee1d939b38596e97c44ebb271de3ca
Le-Caignec <robinlecaignec@yahoo.fr>
2024-02-19 08:51:54 +0100

    dix index.ts export

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,4 +3,4 @@ export { getWeb3Provider } from './utils/getWeb3Provider.js';
 
 export { DataProtector } from './lib/dataProtector/DataProtector.js';
 export { DataProtectorSharing } from './lib/dataProtectorSharing/DataProtectorSharing.js';
-export { IExecDataProtector } from './lib/IExecDataProtector.js';
+export { IExecDataProtector } from './lib/IExecDataprotector.js';
