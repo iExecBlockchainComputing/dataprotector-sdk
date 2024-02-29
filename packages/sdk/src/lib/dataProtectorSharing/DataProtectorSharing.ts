49c661772cf27abdfebef13f0cb509abc6abb21b
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-02-29 14:33:12 +0100

    Add optional chaining for some option that can be undefined

--- packages/sdk/src/lib/dataProtectorSharing/DataProtectorSharing.ts
+++ packages/sdk/src/lib/dataProtectorSharing/DataProtectorSharing.ts
@@ -61,7 +61,7 @@ class DataProtectorSharing extends IExecDataProtectorModule {
   ) {
     super(ethProvider, options);
     this.sharingContractAddress =
-      options?.sharingContractAddress.toLowerCase() ||
+      options?.sharingContractAddress?.toLowerCase() ||
       DEFAULT_SHARING_CONTRACT_ADDRESS;
   }
 
