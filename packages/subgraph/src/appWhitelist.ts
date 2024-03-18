6024aeeae614871f50ec730ebf8ea291b3fef698
Le-Caignec <robinlecaignec@yahoo.fr>
2024-03-18 16:32:13 +0100

    fix subgraph indexing error

--- packages/subgraph/src/appWhitelist.ts
+++ packages/subgraph/src/appWhitelist.ts
@@ -2,9 +2,9 @@ import { NewAppAddedToAppWhitelist as NewAppAddedToAppWhitelistEvent } from '../
 import { App } from '../generated/schema';
 
 export function handleNewAppAdded(event: NewAppAddedToAppWhitelistEvent): void {
-  let app = App.load(event.params.appAddress);
+  let app = App.load(event.params.appAddress.toHex());
   if (!app) {
-    app = new App(event.params.appAddress);
+    app = new App(event.params.appAddress.toHex());
     app.appWhitelist = event.address.toHex();
     app.save();
   }
--- packages/subgraph/src/appWhitelistRegistry.ts
+++ packages/subgraph/src/appWhitelistRegistry.ts
@@ -3,13 +3,13 @@ import { AppWhitelistTemplate } from '../generated/templates';
 import { AppWhitelist } from '../generated/schema';
 import { Address } from '@graphprotocol/graph-ts';
 
-export function handleNewAppContract(event: TransferEvent): void {
+export function handleNewAppWhitelist(event: TransferEvent): void {
   let appWhitelistAddress = event.params.tokenId.toHex();
   let appWhitelist = AppWhitelist.load(appWhitelistAddress);
   if (!appWhitelist) {
     appWhitelist = new AppWhitelist(appWhitelistAddress);
-    // appWhitelist.owner = event.params.to;
+    appWhitelist.owner = event.params.to;
     appWhitelist.save();
   }
-  // AppWhitelistTemplate.create(Address.fromString(appWhitelistAddress));
+  AppWhitelistTemplate.create(Address.fromString(appWhitelistAddress));
 }
