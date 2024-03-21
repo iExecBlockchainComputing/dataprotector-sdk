736fb6d6871e4395174ecc32c3be354d31944c17
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-03-21 12:57:59 +0100

    migrate tests running on prod

--- packages/sdk/tests/e2e/dataProtectorCore/processProtectedData.test.ts
+++ packages/sdk/tests/e2e/dataProtectorCore/processProtectedData.test.ts
@@ -1,31 +1,61 @@
 import { beforeAll, describe, expect, it } from '@jest/globals';
 import { HDNodeWallet, Wallet } from 'ethers';
+import { IExec } from 'iexec';
 import {
   IExecDataProtectorCore,
   ProtectedDataWithSecretProps,
-  getWeb3Provider,
 } from '../../../src/index.js';
 import {
   MAX_EXPECTED_BLOCKTIME,
   MAX_EXPECTED_WEB2_SERVICES_TIME,
+  deployRandomApp,
+  getTestConfig,
 } from '../../test-utils.js';
 
 describe('dataProtectorCore.processProtectedData()', () => {
   let dataProtectorCore: IExecDataProtectorCore;
   let wallet: HDNodeWallet;
   let protectedData: ProtectedDataWithSecretProps;
+  let appAddress: string;
+  let workerpoolAddress: string;
   beforeAll(async () => {
     wallet = Wallet.createRandom();
     dataProtectorCore = new IExecDataProtectorCore(
-      getWeb3Provider(wallet.privateKey)
+      ...getTestConfig(wallet.privateKey)
     );
+    // create app & workerpool
+    const [ethProvider, options] = getTestConfig(wallet.privateKey);
+    appAddress = await deployRandomApp({
+      ethProvider,
+      teeFramework: 'scone',
+    });
+    const iexec = new IExec({ ethProvider }, options.iexecOptions);
+    await iexec.order
+      .createApporder({ app: appAddress, volume: 1000, tag: ['tee', 'scone'] })
+      .then(iexec.order.signApporder)
+      .then(iexec.order.publishApporder);
+    const { address: workerpool } = await iexec.workerpool.deployWorkerpool({
+      description: 'test pool',
+      owner: await iexec.wallet.getAddress(),
+    });
+    workerpoolAddress = workerpool;
+    await iexec.order
+      .createWorkerpoolorder({
+        workerpool: workerpoolAddress,
+        category: 0,
+        volume: 1000,
+        tag: ['tee', 'scone'],
+      })
+      .then(iexec.order.signWorkerpoolorder)
+      .then(iexec.order.publishWorkerpoolorder);
 
+    // create protectedData
     protectedData = await dataProtectorCore.protectData({
       data: { email: 'example@example.com' },
       name: 'test do not use',
     });
     await dataProtectorCore.grantAccess({
-      authorizedApp: '0x4605e8af487897faaef16f0709391ef1be828591',
+      authorizedApp: appAddress,
       protectedData: protectedData.address,
       authorizedUser: wallet.address,
     });
@@ -35,7 +65,8 @@ describe('dataProtectorCore.processProtectedData()', () => {
     async () => {
       const taskId = await dataProtectorCore.processProtectedData({
         protectedData: protectedData.address,
-        app: '0x4605e8af487897faaef16f0709391ef1be828591',
+        app: appAddress,
+        workerpool: workerpoolAddress,
         secrets: {
           1: 'ProcessProtectedData test subject',
           2: 'email content for test processData',
