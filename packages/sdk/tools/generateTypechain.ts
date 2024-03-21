29f92afbadb63f0e8a432d3ce3f674e83a74da23
Le-Caignec <robinlecaignec@yahoo.fr>
2024-03-21 10:03:15 +0100

    try to fix drone permissions on script to generate typechain

--- packages/sdk/tools/generateTypechain.ts
+++ packages/sdk/tools/generateTypechain.ts
@@ -4,8 +4,20 @@ $.verbose = false; // Disable bash commands logging.
 
 async function generateTypechain() {
   console.log(`Generating typechain`);
+
+  console.log('Cleaning up node_modules directories...');
+  await $`rimraf ../sharing-smart-contract/node_modules`;
+  await $`rimraf ../smart-contract/node_modules`;
+
+  console.log(
+    'Installing dependencies and compiling for sharing-smart-contract...'
+  );
   await $`cd ../sharing-smart-contract && npm ci && npm run compile`;
+
+  console.log('Installing dependencies and compiling for smart-contract...');
   await $`cd ../smart-contract && npm ci && npm run compile`;
+
+  console.log('Generating typechain files...');
   await $`rimraf typechain && typechain --target=ethers-v6 --out-dir ./typechain '{../sharing-smart-contract/artifacts/contracts/**,../smart-contract/artifacts/contracts/**}/!(*.dbg).json' --node16-modules`;
 }
 
