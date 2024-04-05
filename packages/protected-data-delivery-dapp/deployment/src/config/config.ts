8e33cd7b37797c9449aab365f5f9296e65084698
luisccastillo <luiscarloscastillo@protonmail.com>
2024-04-05 16:42:32 +0200

    test consuming app old version for dev

--- packages/protected-data-delivery-dapp/deployment/src/config/config.ts
+++ packages/protected-data-delivery-dapp/deployment/src/config/config.ts
@@ -5,7 +5,7 @@ export const DEFAULT_SHARING_CONTRACT_ADDRESS =
 export const HOST = 'https://bellecour.iex.ec';
 
 //deployment parameters
-export const APP_NAME = 'protected-data-delivery-dev-test';
+export const APP_NAME = 'content-consumer-dev-test';
 export const APP_TYPE = 'DOCKER';
 export const FRAMEWORK = 'scone';
 
@@ -15,7 +15,7 @@ export const DEFAULT_APP_VOLUME = 1000000;
 export const APP_TAG = ['tee', 'scone'];
 
 //ENS name
-export const SUBDOMAIN_DEV = 'protected-data-delivery-dev-test';
+export const SUBDOMAIN_DEV = 'content-consumer-dev-test';
 export const SUBDOMAIN_PROD = 'protected-data-delivery';
 export const IEXEC_ENS_DOMAINE = 'apps.iexec.eth';
 
@@ -25,9 +25,9 @@ export const DAPP_ENS_NAME_PROD = `${SUBDOMAIN_PROD}.${IEXEC_ENS_DOMAINE}`;
 //const SCONIFIER_VERSION = '5.7.5-v14';
 
 export const DOCKER_IMAGE_NAMESPACE = 'iexechub';
-export const DOCKER_IMAGE_REPOSITORY = 'protected-data-delivery-dapp';
+export const DOCKER_IMAGE_REPOSITORY = 'content-creator-dapp';
 //export const DOCKER_IMAGE_DEV_TAG = `dev-${process.env.DRONE_COMMIT}-sconify-${SCONIFIER_VERSION}-dev`;
-export const DOCKER_IMAGE_DEV_TAG = `dev-6e1ba4135e90294bfde1f3a073bf034d75f62a32-sconify-5.7.5-v14-debug`;
+export const DOCKER_IMAGE_DEV_TAG = `0.0.0-sconify-5.7.5-v12-production`;
 export const DOCKER_IMAGE_PROD_TAG = ``;
 
 //drone target
