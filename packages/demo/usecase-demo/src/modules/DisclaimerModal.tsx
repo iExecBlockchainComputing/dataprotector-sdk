24c6839d4fc59c0bf02153da8721d545cfe388ae
“Erwan <erwan.decoster@iex.ec>
2024-04-10 11:19:33 +0200

    Update modal style

--- packages/demo/usecase-demo/src/modules/DisclaimerModal.tsx
+++ packages/demo/usecase-demo/src/modules/DisclaimerModal.tsx
@@ -5,7 +5,6 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog.tsx';
 import { Label } from '@/components/ui/label';
@@ -42,31 +41,33 @@ export function DisclaimerModal() {
   }
 
   if (isStorageDisclaimerViewed) {
-    return false
+    return false;
   }
 
   return (
     <Dialog open={open} onOpenChange={onModalOpenChange}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Disclaimer</DialogTitle>
-          <DialogDescription>
-            This dApp is a demo to learn the methods and see them in action.
-            It is not a Content Creator application for creating and
-            distributing Web3 content.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4">
-          <h4 className="text-xl font-bold">Try developper mode</h4>
-          <div className="flex items-center space-x-2">
-            <Switch
-              id="dev-mode"
-              checked={isDevMode}
-              onCheckedChange={setDevMode}
-            />
-            <Label htmlFor="dev-mode">
-              Use the developer mode switch to view methods.{' '}
-            </Label>
+      <DialogContent className="pt-6">
+        <DialogDescription className='pb-6'>
+          <div className="grid gap-4">
+            <DialogTitle>Disclaimer</DialogTitle>
+            <p>
+              This dApp is a demo to learn the methods and see them in action.
+              It is not a Content Creator application for creating and
+              distributing Web3 content.
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <h4 className="text-xl font-bold">Try developer mode</h4>
+            <div className="flex items-center space-x-2">
+              <Switch
+                id="dev-mode"
+                checked={isDevMode}
+                onCheckedChange={setDevMode}
+              />
+              <Label htmlFor="dev-mode">
+                Use the developer mode switch to view methods.{' '}
+              </Label>
+            </div>
           </div>
           <DocLink className={isDevMode ? 'visible' : 'invisible'}>
             dataprotector-sdk / Method called:{' '}
@@ -79,7 +80,7 @@ export function DisclaimerModal() {
               fetchProtectedData()
             </a>
           </DocLink>
-        </div>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
