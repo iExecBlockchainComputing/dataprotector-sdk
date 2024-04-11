4055d55fd409abe1abbc13adcd1a41a6ccc1e29e
Luis Carlos <luiscarloscastillo@protonmail.com>
2024-04-11 15:33:04 +0200

    [Dapp] app and whitelist deployment (#211)
    Co-authored-by: Le-Caignec <robinlecaignec@yahoo.fr>
    Co-authored-by: Luis Carlos <luisccastillo@users.noreply.github.com>
    Co-authored-by: Robin Le Caignec <72495599+Le-Caignec@users.noreply.github.com>

---
+++ packages/protected-data-delivery-dapp/deployment/src/singleFunction/addAppToWhitelist.ts
@@ -0,0 +1,30 @@
+import { Contract } from 'ethers';
+import { IExec } from 'iexec';
+import * as APP_WHITELIST_ABI from '../../abis/AppWhitelistABI.json';
+
+const addAppToWhitelist = async (
+  iexec: IExec,
+  appWhitelistContractAddress: string,
+  deliveryAppAddress: string
+): Promise<string> => {
+  try {
+    const { signer } = await iexec.config.resolveContractsClient();
+
+    const appWhitelistContract = new Contract(
+      appWhitelistContractAddress,
+      APP_WHITELIST_ABI.default,
+      signer
+    );
+
+    const addAppTx = await appWhitelistContract.addApp(deliveryAppAddress);
+    const addAppReceipt = await addAppTx.wait();
+    console.log(
+      `Added App ${deliveryAppAddress} to whitelist ${appWhitelistContractAddress}  (tx: ${addAppReceipt.hash})`
+    );
+    return appWhitelistContractAddress;
+  } catch (error) {
+    console.error(`Error adding app to whitelist: ${error.message}`);
+  }
+};
+
+export default addAppToWhitelist;
