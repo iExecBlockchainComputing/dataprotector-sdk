4fc9df355a29bfd5b98b7d1632898a0a28c3aa66
Le-Caignec <robinlecaignec@yahoo.fr>
2024-04-12 10:47:30 +0200

    change dapp address (using scone v12)

--- packages/sdk/src/config/config.ts
+++ packages/sdk/src/config/config.ts
@@ -24,7 +24,7 @@ export const DEFAULT_PROTECTED_DATA_SHARING_APP_WHITELIST =
   '0xba46d69dd9fdf361c324aa93decd3ffd55514cd1'.toLowerCase();
 
 export const DEFAULT_PROTECTED_DATA_SHARING_APP =
-  '0xE84cfb2D0D8BAd03713c8740984161d155f4f574'.toLowerCase();
+  '0xa7d1A70099df114952754e27bF1d89F1EaB7F81a'.toLowerCase();
 
 export const DEFAULT_SUBGRAPH_URL =
   'https://thegraph-product.iex.ec/subgraphs/name/bellecour/dev-dataprotector-v2';
--- packages/sdk/tests/e2e/dataProtectorSharing/consumeProtectedData.test.ts
+++ packages/sdk/tests/e2e/dataProtectorSharing/consumeProtectedData.test.ts
@@ -5,7 +5,7 @@ import { IExecDataProtector } from '../../../src/index.js';
 import { timeouts } from '../../test-utils.js';
 
 const WORKERPOOL_ADDRESS = 'prod-stagingv8.main.pools.iexec.eth';
-const APP_ADDRESS = '0xE84cfb2D0D8BAd03713c8740984161d155f4f574';
+const APP_ADDRESS = '0xa7d1A70099df114952754e27bF1d89F1EaB7F81a';
 
 describe('dataProtector.consumeProtectedData()', () => {
   let dataProtectorCreator: IExecDataProtector;
