6615f951850e95bdca86696571b3e70130f31cdb
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-04-17 16:52:36 +0200

    update environments on deploy

--- packages/sharing-smart-contract/utils/utils.js
+++ packages/sharing-smart-contract/utils/utils.js
@@ -2,28 +2,29 @@ import fs from 'fs/promises';
 
 // if you change the name of the file, you need to change the name in the pipeline too
 const SMART_CONTRACT_ADDRESS_FILE_SUFFIX = '.smart-contract-address';
+// if you change the name of the file, you need to change the name in the pipeline too
+const CONSTRUCTOR_ARGS_PARAMS_FILE_SUFFIX = '.constructor-args-params';
+const APP_WHITELIST_REGISTRY = 'AppWhitelistRegistry';
+const DATA_PROTECTOR_SHARING = 'DataProtectorSharing';
 
 /**
- * save the app address in `.smart-contract-address` file for next usages
+ * save the app address in `ContractName.smart-contract-address` file for next usages
  */
 const saveSmartContractAddress = (contractName) => (address) =>
   fs.writeFile(`${contractName}${SMART_CONTRACT_ADDRESS_FILE_SUFFIX}`, address);
 
 export const saveDataProtectorSharingContractAddress =
-  saveSmartContractAddress('DataProtectorSharing');
+  saveSmartContractAddress(DATA_PROTECTOR_SHARING);
 export const saveAppWhitelistRegistryContractAddress =
-  saveSmartContractAddress('AppWhitelistRegistry');
-
-// if you change the name of the file, you need to change the name in the pipeline too
-const CONSTRUCTOR_ARGS_PARAMS_FILE_SUFFIX = '.constructor-args-params';
+  saveSmartContractAddress(APP_WHITELIST_REGISTRY);
 
 /**
- * save the app address in `.constructor-arg-params` file for next usages
+ * save the app address in `ContractName.constructor-arg-params` file for next usages
  */
 const saveConstructorArgsParams = (contractName) => (args) =>
   fs.writeFile(`${contractName}${CONSTRUCTOR_ARGS_PARAMS_FILE_SUFFIX}`, args);
 
 export const saveDataProtectorSharingConstructorArgsParams =
-  saveConstructorArgsParams('DataProtectorSharing');
+  saveConstructorArgsParams(DATA_PROTECTOR_SHARING);
 export const saveAppWhitelistRegistryConstructorArgsParams =
-  saveConstructorArgsParams('AppWhitelistRegistry');
+  saveConstructorArgsParams(APP_WHITELIST_REGISTRY);
