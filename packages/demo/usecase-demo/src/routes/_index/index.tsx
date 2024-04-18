92d7f15acbcdf5c6abde97a163d836a94fb09d10
“Erwan <erwan.decoster@iex.ec>
2024-04-18 17:53:36 +0200

    improve index page responsive

--- packages/demo/usecase-demo/src/routes/_index/index.tsx
+++ packages/demo/usecase-demo/src/routes/_index/index.tsx
@@ -21,18 +21,18 @@ function Home() {
           <source src={headerMotionUrl} type="video/mp4" />
         </video>
         <div className="px-10 text-white">
-          <div className="scale-y-100 text-balance font-anybody text-5xl font-bold leading-[3rem]">
+          <div className="scale-y-100 text-balance font-anybody text-2xl font-bold sm:text-5xl sm:leading-[3rem]">
             Share your content
             <br />
             Earn crypto
           </div>
-          <div className="mt-10 flex scale-y-100 items-center justify-center">
+          <div className="mt-6 grid scale-y-100 items-center justify-center gap-4 sm:mt-10 sm:flex">
             <span className="text-grey-50">Powered by</span>
             <a
               href="https://documentation-tools.vercel.app/tools/dataProtector.html"
               target="_blank"
               rel="noopener"
-              className="text-md ml-4 flex items-center rounded-30 border border-yellow-200 px-6 py-3 font-grotesk font-bold text-primary"
+              className="text-md flex items-center rounded-30 border border-yellow-200 px-6 py-3 font-grotesk font-bold text-primary"
             >
               Data Protector
             </a>
@@ -42,7 +42,7 @@ function Home() {
 
       <div className="mt-20">
         <div className="text-[#D9D9D9]">Welcome to Content Creator 👋</div>
-        <h1 className="mt-1 font-anybody text-5xl font-[750]">
+        <h1 className="mt-1 text-balance font-anybody text-2xl font-[750] sm:text-5xl">
           Discover all the new
           <br /> content you need
         </h1>
