392b0a7a097cfcb8b4ed22b20c25cb5b9b926b8b
ErwanDecoster <wanerd2@gmail.com>
2024-04-23 12:16:15 +0200

    Add modal border gradient

--- packages/demo/usecase-demo/src/components/ui/dialog.tsx
+++ packages/demo/usecase-demo/src/components/ui/dialog.tsx
@@ -35,7 +35,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 w-[92%] max-w-lg translate-x-[-50%] translate-y-[-50%] rounded-lg border border-grey-700 bg-grey-800 shadow-lg duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:w-full sm:rounded-xl',
+        'fixed left-[50%] top-[50%] z-50 w-[92%] max-w-lg translate-x-[-50%] translate-y-[-50%] rounded-xl bg-gradient-to-b from-gray-500 to-background shadow-lg duration-300 before:absolute before:inset-px before:-z-10 before:rounded-[11px] before:bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:w-full sm:rounded-3xl sm:before:rounded-[23px]',
         className
       )}
       {...props}
