c88e7c1c3a912ee0d98225d0f35dc5f3097082ae
Le-Caignec <robinlecaignec@yahoo.fr>
2024-04-25 09:03:11 +0200

    add the verify task for the upgrade script to verify the new implementation

--- packages/sharing-smart-contract/scripts/upgrade.js
+++ packages/sharing-smart-contract/scripts/upgrade.js
@@ -14,12 +14,17 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log('Deploying contracts with the account:', deployer.address);
 
-  // pass the registry instance to the deploy method
+  const dataProtectorSharingConstructorArgs = [
+    POCO_PROXY_ADDRESS,
+    POCO_PROTECTED_DATA_REGISTRY_ADDRESS,
+    AddOnlyAppWhitelistRegistryAddress,
+  ];
 
+  // pass the registry instance to the deploy method
   const DataProtectorSharingFactoryV2 = await ethers.getContractFactory('DataProtectorSharing');
   const proxyUpgrade = await upgrades.upgradeProxy(dataprotectorSharingContractAddress, DataProtectorSharingFactoryV2, {
     kind: 'transparent',
-    constructorArgs: [POCO_PROXY_ADDRESS, POCO_PROTECTED_DATA_REGISTRY_ADDRESS, AddOnlyAppWhitelistRegistryAddress],
+    constructorArgs: dataProtectorSharingConstructorArgs,
   });
 
   await proxyUpgrade.waitForDeployment();
@@ -29,6 +34,16 @@ async function main() {
     'NEW Implementation address (DataProtectorSharing.sol):',
     await upgrades.erc1967.getImplementationAddress(dataprotectorSharingContractAddress),
   );
+
+  // Verify smart-contract
+  try {
+    await hre.run('verify:verify', {
+      address: proxyUpgrade,
+      constructorArguments: dataProtectorSharingConstructorArgs,
+    });
+  } catch (e) {
+    console.log('Proxy verification for DataProtectorSharingContract may have failed :', e);
+  }
 }
 
 main().catch(error => {
