f069b18fd8e1ce5ce67a5c289fa10dd9b42479b9
ErwanDecoster <wanerd2@gmail.com>
2024-05-20 18:21:45 +0200

    responsive improvement

--- packages/demo/usecase-demo/src/components/DocLink.tsx
+++ packages/demo/usecase-demo/src/components/DocLink.tsx
@@ -17,14 +17,14 @@ export function DocLink({
       className={
         isDevMode
           ? cn(
-              'visible flex items-start rounded bg-grey-700 px-4 py-3 font-mono text-sm font-normal tracking-tighter text-grey-100',
+              'visible flex items-start rounded bg-grey-700 px-4 py-3 font-mono text-sm font-normal tracking-tighter text-grey-100 overflow-x-scroll',
               className
             )
           : 'invisible h-0'
       }
     >
-      <BookOpen size="20" className="mt-px inline-block" />
-      <div className="ml-3">{children}</div>
+      <BookOpen size="20" className="mt-px hidden md:inline-block" />
+      <div className="md:ml-3">{children}</div>
     </div>
   );
 }
