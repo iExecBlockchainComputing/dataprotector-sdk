d5150ae99b632d3fb6d2af98a5a07945ed1781a7
Le-Caignec <robinlecaignec@yahoo.fr>
2024-05-20 15:32:10 +0200

    create Reader class

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,9 +2,22 @@ export * from './lib/types/index.js';
 export { getWeb3Provider } from './utils/getWeb3Provider.js';
 export * from './utils/index.js';
 
+// -------------------------------------------------------
+//For READ & WRITE
+// -------------------------------------------------------
 // umbrella
-export { IExecDataProtectorCore } from './lib/dataProtectorCore/IExecDataProtectorCore.js';
+export { IExecDataProtector } from './lib/IExecDataProtector.js';
 
 // submodules only
 export { IExecDataProtectorSharing } from './lib/dataProtectorSharing/IExecDataProtectorSharing.js';
-export { IExecDataProtector } from './lib/IExecDataprotector.js';
+export { IExecDataProtectorCore } from './lib/dataProtectorCore/IExecDataProtectorCore.js';
+
+// -------------------------------------------------------
+//For READ only
+// -------------------------------------------------------
+// umbrella
+export { IExecDataProtectorReader } from './lib/IExecDataProtectorReader.js';
+
+// submodules only
+export { IExecDataProtectorSharingReader } from './lib/dataProtectorSharing/IExecDataProtectorSharingReader.js';
+export { IExecDataProtectorCoreReader } from './lib/dataProtectorCore/IExecDataProtectorCoreReader.js';
