d38f39974174160b3677bae5b34302469e4b6545
luisccastillo <luiscarloscastillo@protonmail.com>
2024-05-22 14:51:55 +0200

    remove inecessary code from sdk config options

--- packages/sdk/tests/scripts/prepare-bellecour-fork-for-tests.js
+++ packages/sdk/tests/scripts/prepare-bellecour-fork-for-tests.js
@@ -6,14 +6,12 @@ import {
   keccak256,
   toBeHex,
 } from 'ethers';
+
 // eslint-disable-next-line import/extensions
 import { VOUCHER_HUB_ADDRESS } from '../bellecour-fork/voucher-config.js'; // TODO: change with deployment address once voucher is deployed on bellecour
 
 const { DRONE } = process.env;
 
-const IEXEC_HUB_ADDRESS = '0x3eca1B216A7DF1C7689aEb259fFB83ADFB894E7f';
-const TARGET_POCO_ADMIN_WALLET = '0x7bd4783FDCAD405A28052a0d1f11236A741da593';
-const TARGET_FAUCET_WALLET = '0xdFa2585C16cAf9c853086F36d2A37e9b8d1eab87';
 const TARGET_VOUCHER_MANAGER_WALLET =
   '0x44cA21A3c4efE9B1A0268e2e9B2547E7d9C8f19C';
 const DEBUG_WORKERPOOL_OWNER_WALLET =
@@ -74,44 +72,6 @@ const stopImpersonate = async (address) => {
     },
   });
   console.log(`stop impersonating ${address}`);
-};
-
-const getIExecHubOwnership = async (targetOwner) => {
-  const iexecContract = new Contract(
-    IEXEC_HUB_ADDRESS,
-    [
-      {
-        inputs: [],
-        name: 'owner',
-        outputs: [{ internalType: 'address', name: '', type: 'address' }],
-        stateMutability: 'view',
-        type: 'function',
-        constant: true,
-      },
-      {
-        inputs: [
-          { internalType: 'address', name: 'newOwner', type: 'address' },
-        ],
-        name: 'transferOwnership',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-    ],
-    provider,
-  );
-  const iexecOwner = await iexecContract.owner();
-  await impersonate(iexecOwner);
-  await iexecContract
-    .connect(new JsonRpcSigner(provider, iexecOwner))
-    .transferOwnership(targetOwner, { gasPrice: 0 })
-    .then((tx) => tx.wait());
-  await stopImpersonate(iexecOwner);
-
-  const newOwner = await iexecContract.owner();
-  console.log(
-    `IExecHub proxy at ${IEXEC_HUB_ADDRESS} is now owned by ${newOwner}`,
-  );
 };
 
 const getVoucherManagementRoles = async (targetManager) => {
@@ -302,13 +262,6 @@ const getWorkerpoolOwnership = async (resourceAddress, targetOwner) => {
 
 const main = async () => {
   console.log(`preparing bellecour-fork at ${rpcURL}`);
-
-  // prepare PoCo
-  await setBalance(TARGET_POCO_ADMIN_WALLET, 1000000n * 10n ** 18n);
-  await getIExecHubOwnership(TARGET_POCO_ADMIN_WALLET);
-
-  // prepare faucet wallet
-  await setBalance(TARGET_FAUCET_WALLET, 1000000n * 10n ** 18n);
 
   // prepare Voucher
   await setBalance(TARGET_VOUCHER_MANAGER_WALLET, 1000000n * 10n ** 18n);
--- packages/sdk/tests/test-utils.ts
+++ packages/sdk/tests/test-utils.ts
@@ -1,11 +1,19 @@
-import { Wallet } from 'ethers';
-import { IExecAppModule, TeeFramework, utils } from 'iexec';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { Wallet, JsonRpcProvider, ethers, Contract } from 'ethers';
+import { IExec, IExecAppModule, TeeFramework, utils } from 'iexec';
 import {
   DataProtectorConfigOptions,
   Web3SignerProvider,
   getWeb3Provider,
 } from '../src/index.js';
+// eslint-disable-next-line import/extensions
+import { VOUCHER_HUB_ADDRESS } from './bellecour-fork/voucher-config.js';
 import { WAIT_FOR_SUBGRAPH_INDEXING } from './unit/utils/waitForSubgraphIndexing.js';
+import { getSignerFromPrivateKey } from 'iexec/utils';
+import { getEventFromLogs } from '../src/utils/transactionEvent.js';
+
+const { DRONE } = process.env;
 
 export const getTestWeb3SignerProvider = (
   privateKey: string
@@ -15,7 +23,7 @@ export const getTestWeb3SignerProvider = (
     privateKey
   );
 
-const getTestIExecOption = () => ({
+export const getTestIExecOption = () => ({
   smsURL: process.env.DRONE ? 'http://sms:13300' : 'http://127.0.0.1:13300',
   resultProxyURL: process.env.DRONE
     ? 'http://result-proxy:13200'
@@ -38,8 +46,55 @@ export const getTestConfig = (
     subgraphUrl: process.env.DRONE
       ? 'http://graphnode:8000/subgraphs/name/DataProtector-v2'
       : 'http://127.0.0.1:8000/subgraphs/name/DataProtector-v2',
+    voucherHubAddress: VOUCHER_HUB_ADDRESS,
+    voucherSubgraphURL: DRONE
+      ? 'http://gaphnode:8000/subgraphs/name/bellecour/iexec-voucher'
+      : 'http://localhost:8000/subgraphs/name/bellecour/iexec-voucher', // TODO: change with deployment address once voucher is deployed on bellecour
   };
   return [ethProvider, options];
+};
+
+export const getTestChainConfig = () => {
+  return {
+    rpcURL: DRONE ? 'http://bellecour-fork:8545' : 'http://localhost:8545',
+    chainId: '134',
+    iexecGatewayURL: DRONE ? 'http://market-api:3000' : 'http://localhost:3000',
+    resultProxyURL: DRONE
+      ? 'http://result-proxy:13200'
+      : 'http://localhost:13200',
+    pocoAdminWallet: new Wallet(
+      '0x564a9db84969c8159f7aa3d5393c5ecd014fce6a375842a45b12af6677b12407'
+    ),
+    faucetWallet: new Wallet(
+      '0xde43b282c2931fc41ca9e1486fedc2c45227a3b9b4115c89d37f6333c8816d89'
+    ),
+    voucherHubAddress: VOUCHER_HUB_ADDRESS, // TODO: change with deployment address once voucher is deployed on bellecour
+    voucherManagerWallet: new Wallet(
+      '0x2c906d4022cace2b3ee6c8b596564c26c4dcadddf1e949b769bcb0ad75c40c33'
+    ),
+    voucherSubgraphURL: DRONE
+      ? 'http://gaphnode:8000/subgraphs/name/bellecour/iexec-voucher'
+      : 'http://localhost:8000/subgraphs/name/bellecour/iexec-voucher',
+    debugWorkerpool: 'debug-v8-bellecour.main.pools.iexec.eth',
+    debugWorkerpoolOwnerWallet: new Wallet(
+      '0x800e01919eadf36f110f733decb1cc0f82e7941a748e89d7a3f76157f6654bb3'
+    ),
+    prodWorkerpool: 'prod-v8-bellecour.main.pools.iexec.eth',
+    prodWorkerpoolOwnerWallet: new Wallet(
+      '0x6a12f56d7686e85ab0f46eb3c19cb0c75bfabf8fb04e595654fc93ad652fa7bc'
+    ),
+    provider: new JsonRpcProvider(
+      DRONE ? 'http://bellecour-fork:8545' : 'http://localhost:8545'
+    ),
+    defaults: {
+      hubAddress: '0x3eca1B216A7DF1C7689aEb259fFB83ADFB894E7f',
+      ensRegistryAddress: '0x5f5B93fca68c9C79318d1F3868A354EE67D8c006',
+      ensPublicResolverAddress: '0x1347d8a1840A810B990d0B774A6b7Bb8A1bd62BB',
+      isNative: true,
+      useGas: false,
+      name: 'bellecour',
+    },
+  };
 };
 
 export const getRequiredFieldMessage = (field: string = 'this') =>
@@ -401,7 +456,265 @@ export const MOCK_WORKERPOOL_ORDER = {
   ],
   count: 2,
 };
+
 export const EMPTY_ORDER_BOOK: any = {
   orders: [],
   count: 0,
 };
+
+export const sleep = (ms) =>
+  new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, ms);
+  });
+
+export const setBalance = (chain) => async (address, targetWeiBalance) => {
+  await fetch(chain.rpcURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      method: 'anvil_setBalance',
+      params: [address, ethers.toBeHex(targetWeiBalance)],
+      id: 1,
+      jsonrpc: '2.0',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const setNRlcBalance = (chain) => async (address, nRlcTargetBalance) => {
+  const weiAmount = BigInt(`${nRlcTargetBalance}`) * 10n ** 9n; // 1 nRLC is 10^9 wei
+  await setBalance(chain)(address, weiAmount);
+};
+
+export const createVoucherType =
+  (chain) =>
+  async ({ description = 'test', duration = 1000 } = {}) => {
+    const VOUCHER_HUB_ABI = [
+      {
+        inputs: [
+          {
+            internalType: 'string',
+            name: 'description',
+            type: 'string',
+          },
+          {
+            internalType: 'uint256',
+            name: 'duration',
+            type: 'uint256',
+          },
+        ],
+        name: 'createVoucherType',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: true,
+            internalType: 'uint256',
+            name: 'id',
+            type: 'uint256',
+          },
+          {
+            indexed: false,
+            internalType: 'string',
+            name: 'description',
+            type: 'string',
+          },
+          {
+            indexed: false,
+            internalType: 'uint256',
+            name: 'duration',
+            type: 'uint256',
+          },
+        ],
+        name: 'VoucherTypeCreated',
+        type: 'event',
+      },
+    ];
+    const voucherHubContract = new Contract(
+      chain.voucherHubAddress,
+      VOUCHER_HUB_ABI,
+      chain.provider
+    );
+    const signer = chain.voucherManagerWallet.connect(chain.provider);
+    const createVoucherTypeTxHash = await voucherHubContract
+      .connect(signer)
+      .createVoucherType(description, duration);
+    const txReceipt = await createVoucherTypeTxHash.wait();
+    const { id } = getEventFromLogs('VoucherTypeCreated', txReceipt.logs, {
+      strict: true,
+    }).args;
+
+    return id;
+  };
+
+// TODO: update createWorkerpoolorder() parameters when it is specified
+const createAndPublishWorkerpoolOrder = async (
+  chain,
+  workerpool,
+  workerpoolOwnerWallet,
+  voucherOwnerAddress
+) => {
+  const ethProvider = utils.getSignerFromPrivateKey(
+    chain.rpcURL,
+    workerpoolOwnerWallet.privateKey
+  );
+  const iexec = new IExec(
+    { ethProvider },
+    { iexecGatewayURL: chain.iexecGatewayURL }
+  );
+
+  const workerpoolprice = 1000;
+  const volume = 1000;
+
+  await setNRlcBalance(chain)(
+    await iexec.wallet.getAddress(),
+    volume * workerpoolprice
+  );
+  await iexec.account.deposit(volume * workerpoolprice);
+
+  const workerpoolorder = await iexec.order.createWorkerpoolorder({
+    workerpool,
+    category: 0,
+    requesterrestrict: voucherOwnerAddress,
+    volume,
+    workerpoolprice,
+    tag: ['tee', 'scone'],
+  });
+
+  await iexec.order
+    .signWorkerpoolorder(workerpoolorder)
+    .then((o) => iexec.order.publishWorkerpoolorder(o));
+};
+
+export const createVoucher =
+  (chain) =>
+  async ({ owner, voucherType, value }) => {
+    const VOUCHER_HUB_ABI = [
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: 'owner',
+            type: 'address',
+          },
+          {
+            internalType: 'uint256',
+            name: 'voucherType',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'value',
+            type: 'uint256',
+          },
+        ],
+        name: 'createVoucher',
+        outputs: [
+          {
+            internalType: 'address',
+            name: 'voucherAddress',
+            type: 'address',
+          },
+        ],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: 'account',
+            type: 'address',
+          },
+        ],
+        name: 'getVoucher',
+        outputs: [
+          {
+            internalType: 'address',
+            name: '',
+            type: 'address',
+          },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ];
+
+    const iexec = new IExec(
+      {
+        ethProvider: getSignerFromPrivateKey(
+          chain.rpcURL,
+          chain.voucherManagerWallet.privateKey
+        ),
+      },
+      { hubAddress: chain.hubAddress }
+    );
+
+    // ensure RLC balance
+    await setNRlcBalance(chain)(await iexec.wallet.getAddress(), value);
+
+    // deposit RLC to voucherHub
+    const contractClient = await iexec.config.resolveContractsClient();
+    const iexecContract = contractClient.getIExecContract();
+
+    try {
+      await iexecContract.depositFor(chain.voucherHubAddress, {
+        value: BigInt(value) * 10n ** 9n,
+        gasPrice: 0,
+      });
+    } catch (error) {
+      console.error('Error depositing RLC:', error);
+      throw error;
+    }
+
+    const voucherHubContract = new Contract(
+      chain.voucherHubAddress,
+      VOUCHER_HUB_ABI,
+      chain.provider
+    );
+
+    const signer = chain.voucherManagerWallet.connect(chain.provider);
+
+    try {
+      const createVoucherTxHash = await voucherHubContract
+        .connect(signer)
+        .createVoucher(owner, voucherType, value);
+
+      await createVoucherTxHash.wait();
+    } catch (error) {
+      console.error('Error creating voucher:', error);
+      throw error;
+    }
+
+    try {
+      await createAndPublishWorkerpoolOrder(
+        chain,
+        chain.debugWorkerpool,
+        chain.debugWorkerpoolOwnerWallet,
+        owner
+      );
+      await createAndPublishWorkerpoolOrder(
+        chain,
+        chain.prodWorkerpool,
+        chain.prodWorkerpoolOwnerWallet,
+        owner
+      );
+    } catch (error) {
+      console.error('Error publishing workerpoolorder:', error);
+      throw error;
+    }
+
+    try {
+      return await voucherHubContract.getVoucher(owner);
+    } catch (error) {
+      console.error('Error getting voucher:', error);
+      throw error;
+    }
+  };
