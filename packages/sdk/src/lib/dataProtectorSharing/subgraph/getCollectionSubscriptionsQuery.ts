c77f67d92c43ce341b14510988b9c9db609eb9af
Le-Caignec <robinlecaignec@yahoo.fr>
2024-05-22 18:57:00 +0200

    cast collectionId toString

--- packages/sdk/src/lib/dataProtectorSharing/subgraph/getCollectionSubscriptionsQuery.ts
+++ packages/sdk/src/lib/dataProtectorSharing/subgraph/getCollectionSubscriptionsQuery.ts
@@ -36,7 +36,7 @@ export const getCollectionSubscriptionsQuery = async ({
   const variables = {
     where: {
       subscriber: subscriberAddress || undefined,
-      collection: collectionId,
+      collection: collectionId.toString(),
       endDate_gte: includePastSubscriptions
         ? undefined
         : Math.floor(new Date().getTime() / 1000),
