65ccc45b4ea0a532e9cff5141418fe3eba4a68d9
Le-Caignec <robinlecaignec@yahoo.fr>
2024-05-22 16:40:30 +0200

    clean index

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,22 +2,9 @@ export * from './lib/types/index.js';
 export { getWeb3Provider } from './utils/getWeb3Provider.js';
 export * from './utils/index.js';
 
-// -------------------------------------------------------
-//For READ & WRITE
-// -------------------------------------------------------
 // umbrella
 export { IExecDataProtector } from './lib/IExecDataProtector.js';
 
 // submodules only
 export { IExecDataProtectorSharing } from './lib/dataProtectorSharing/IExecDataProtectorSharing.js';
 export { IExecDataProtectorCore } from './lib/dataProtectorCore/IExecDataProtectorCore.js';
-
-// -------------------------------------------------------
-//For READ only
-// -------------------------------------------------------
-// umbrella
-export { IExecDataProtectorReader } from './lib/IExecDataProtectorReader.js';
-
-// submodules only
-export { IExecDataProtectorSharingReader } from './lib/dataProtectorSharing/IExecDataProtectorSharingReader.js';
-export { IExecDataProtectorCoreReader } from './lib/dataProtectorCore/IExecDataProtectorCoreReader.js';
