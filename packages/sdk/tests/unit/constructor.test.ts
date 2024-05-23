51a226169d444c2dfc8e239c54fbb2f4617dc4e0
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-05-23 16:44:56 +0200

    Move to unit tests + prepare two additional tests

--- packages/sdk/tests/unit/constructor.test.ts
+++ packages/sdk/tests/unit/constructor.test.ts
@@ -11,13 +11,14 @@ import {
 import { IExecDataProtector, getWeb3Provider } from '../../src/index.js';
 
 describe('IExecDataProtector()', () => {
-  it('should use default ipfs node url when ipfsNode is not provided', async () => {
+  it('should use default ipfs node url when ipfsNode is NOT provided', async () => {
     const dataProtector = new IExecDataProtector(
       getWeb3Provider(Wallet.createRandom().privateKey)
     );
     const ipfsNode = dataProtector['ipfsNode'];
     expect(ipfsNode).toStrictEqual(DEFAULT_IEXEC_IPFS_NODE);
   });
+
   it('should use provided ipfs node url when ipfsNode is provided', async () => {
     const customIpfsNode = 'https://example.com/node';
     const dataProtector = new IExecDataProtector(
@@ -29,13 +30,15 @@ describe('IExecDataProtector()', () => {
     const ipfsNode = dataProtector['ipfsNode'];
     expect(ipfsNode).toStrictEqual(customIpfsNode);
   });
-  it('should use default ipfs gateway url when ipfsGateway is not provided', async () => {
+
+  it('should use default ipfs gateway url when ipfsGateway is NOT provided', async () => {
     const dataProtector = new IExecDataProtector(
       getWeb3Provider(Wallet.createRandom().privateKey)
     );
     const ipfsGateway = dataProtector['ipfsGateway'];
     expect(ipfsGateway).toStrictEqual(DEFAULT_IPFS_GATEWAY);
   });
+
   it('should use default ipfs gateway url when ipfsGateway is provided', async () => {
     const customIpfsGateway = 'https://example.com/ipfs_gateway';
     const dataProtector = new IExecDataProtector(
@@ -47,7 +50,8 @@ describe('IExecDataProtector()', () => {
     const ipfsGateway = dataProtector['ipfsGateway'];
     expect(ipfsGateway).toStrictEqual(customIpfsGateway);
   });
-  it('should use default smart contract address when dataprotectorContractAddress is not provided', async () => {
+
+  it('should use default smart contract address when dataprotectorContractAddress is NOT provided', async () => {
     const dataProtector = new IExecDataProtector(
       getWeb3Provider(Wallet.createRandom().privateKey)
     );
@@ -57,6 +61,7 @@ describe('IExecDataProtector()', () => {
       DEFAULT_CONTRACT_ADDRESS
     );
   });
+
   it('should use provided smart contract address when dataprotectorContractAddress is provided', async () => {
     const customSContractAddress = Wallet.createRandom().address;
     const dataProtector = new IExecDataProtector(
@@ -71,13 +76,15 @@ describe('IExecDataProtector()', () => {
       customSContractAddress.toLowerCase()
     );
   });
-  it('should use default subgraph URL when subgraphUrl is not provided', async () => {
+
+  it('should use default subgraph URL when subgraphUrl is NOT provided', async () => {
     const dataProtector = new IExecDataProtector(
       getWeb3Provider(Wallet.createRandom().privateKey)
     );
     const graphQLClientUrl = dataProtector['graphQLClient'];
     expect(graphQLClientUrl['url']).toBe(DEFAULT_SUBGRAPH_URL);
   });
+
   it('should use provided subgraph URL when subgraphUrl is provided', async () => {
     const customSubgraphUrl = 'https://example.com/custom-subgraph';
     const dataProtector = new IExecDataProtector(
@@ -89,6 +96,7 @@ describe('IExecDataProtector()', () => {
     const graphQLClient = dataProtector['graphQLClient'];
     expect(graphQLClient['url']).toBe(customSubgraphUrl);
   });
+
   it('should use provided options', async () => {
     const customSubgraphUrl = 'https://example.com/custom-subgraph';
     const customIpfsGateway = 'https://example.com/ipfs_gateway';
@@ -125,6 +133,7 @@ describe('IExecDataProtector()', () => {
     expect(await iexec.config.resolveSmsURL()).toBe(smsURL);
     expect(await iexec.config.resolveIexecGatewayURL()).toBe(iexecGatewayURL);
   }, 20_000);
+
   it('throw when instantiated with an invalid ethProvider', async () => {
     const invalidProvider: any = {};
     expect(() => new IExecDataProtector(invalidProvider)).toThrow(
@@ -155,5 +164,29 @@ describe('IExecDataProtector()', () => {
     );
     const iexec = dataProtector['iexec'];
     expect(await iexec.config.resolveSmsURL()).toBe(smsURL);
+  });
+
+  describe('When instantiating SDK without a signer', () => {
+    describe('When calling a read method', () => {
+      it('should work as expected', () => {
+        // --- GIVEN
+
+        // --- WHEN
+
+        // --- THEN
+        expect();
+      });
+    });
+
+    describe('When calling a write method', () => {
+      it('should throw the corresponding exception', () => {
+        // --- GIVEN
+
+        // --- WHEN
+
+        // --- THEN
+        expect();
+      });
+    });
   });
 });
