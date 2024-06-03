86e88ad3cb0d56e0626458cca1b83dd4e242903d
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-06-03 14:53:59 +0200

    Revert "Fix avatar profile pic logic"
    This reverts commit 98b4e4f384bfb7f4c769012f07cbf4064dbaabf4.

--- packages/demo/usecase-demo/src/utils/getAvatarVisualNumber.ts
+++ packages/demo/usecase-demo/src/utils/getAvatarVisualNumber.ts
@@ -7,14 +7,7 @@ export function getAvatarVisualNumber({
     return 'profile-avatar-bg-1';
   }
   const slicedAddress = address.slice(2);
-  // Get all groups of digits
-  const groups = slicedAddress.match(/\d+/g);
-  if (!groups) {
-    return 'profile-avatar-bg-1';
-  }
-  // Sum each group
-  const sum = groups.reduce((acc, group) => acc + parseInt(group, 10), 0);
-  const chosenImageIndex = sum % 13;
+  const chosenImageIndex = parseInt(slicedAddress, 10) % 13;
   if (chosenImageIndex < 1 || chosenImageIndex > 13) {
     return 'profile-avatar-bg-1';
   }
