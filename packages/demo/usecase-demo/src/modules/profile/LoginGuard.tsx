1b1b1e506ccd9bf802f56e65ecdcee7f739d0a4c
ErwanDecoster <wanerd2@gmail.com>
2024-09-17 10:42:19 +0200

    remove unnecessary code

--- packages/demo/usecase-demo/src/modules/profile/LoginGuard.tsx
+++ packages/demo/usecase-demo/src/modules/profile/LoginGuard.tsx
@@ -1,13 +1,10 @@
 import { useNavigate } from '@tanstack/react-router';
 import { FC, ReactNode, useEffect } from 'react';
-import { AlertCircle } from 'react-feather';
-import { useSwitchChain, useAccount } from 'wagmi';
-import { Button } from '@/components/ui/button.tsx';
+import { useAccount } from 'wagmi';
 import { useUserStore } from '@/stores/user.store.ts';
 
 const LoginGuard: FC<{ children: ReactNode }> = ({ children }) => {
   const { isInitialized, isConnected } = useUserStore();
-  const { chains, error, isPending, switchChain } = useSwitchChain();
   const { chain } = useAccount();
 
   const navigate = useNavigate();
@@ -26,27 +23,6 @@ const LoginGuard: FC<{ children: ReactNode }> = ({ children }) => {
     <>
       {isInitialized && isConnected && chain && chain.id === 134 && (
         <>{children}</>
-      )}
-      {isInitialized && isConnected && chain && chain.id !== 134 && (
-        <div className="mx-auto my-12 ml-12">
-          <p>Oops, you're on the wrong network</p>
-          <p>Click on the following button to switch to the right network</p>
-          <Button
-            disabled={!isPending || chain?.id === chains[0]?.id}
-            key={chains[0]?.id}
-            onClick={() => switchChain({ chainId: chains[0]?.id })}
-            className="mt-4"
-          >
-            Switch to {chains[0].name}
-            {isPending && ' (switching)'}
-          </Button>
-          {error && (
-            <div className="ml-1 mt-1.5 flex items-center text-red-500">
-              <AlertCircle size="14" />
-              <span className="ml-1 text-sm">{error.message}</span>
-            </div>
-          )}
-        </div>
       )}
     </>
   );
