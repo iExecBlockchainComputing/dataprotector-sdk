6f1592c3e7d95ce465706f4b3c360d74799b7e12
ErwanDecoster <wanerd2@gmail.com>
2024-09-19 14:45:07 +0200

    return null instead of empty string

--- packages/sdk/src/utils/getMultiaddrAsString.ts
+++ packages/sdk/src/utils/getMultiaddrAsString.ts
@@ -10,17 +10,17 @@ import { multiaddr } from '@multiformats/multiaddr';
 export function getMultiaddrAsString({
   multiaddrAsHexString,
 }: {
-  multiaddrAsHexString: string;
+  multiaddrAsHexString?: string;
 }) {
   if (!multiaddrAsHexString || multiaddrAsHexString.length < 3) {
-    return '';
+    return null;
   }
   const multiAddrAsBytesArray = multiaddrAsHexString
     .slice(2)
     .match(/.{1,2}/g)
     ?.map((byte) => parseInt(byte, 16));
   if (!multiAddrAsBytesArray) {
-    return '';
+    return null;
   }
   try {
     const multiaddrAsBytes = new Uint8Array(multiAddrAsBytesArray);
@@ -31,6 +31,6 @@ export function getMultiaddrAsString({
       `[getMultiaddrAsString] ERROR: "${multiaddrAsHexString}" is not a valid hex input string?`,
       err
     );
-    return '';
+    return null;
   }
 }
