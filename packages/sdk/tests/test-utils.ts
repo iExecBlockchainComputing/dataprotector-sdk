28a0d736789e40cd64b21c3502c340ca40bf0d98
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-10-01 11:35:09 +0200

    Move unit tests out of e2e folder + add some more with proper iexec-sdk mocks

--- packages/sdk/tests/test-utils.ts
+++ packages/sdk/tests/test-utils.ts
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import { Wallet, JsonRpcProvider, ethers } from 'ethers';
 import { IExecAppModule, IExecConfig, TeeFramework, utils } from 'iexec';
 import {
@@ -54,6 +55,18 @@ export const getRequiredFieldMessage = (field: string = 'this') =>
   `${field} is a required field`;
 
 export const getRandomAddress = () => Wallet.createRandom().address;
+
+export const getRandomTxHash = () => {
+  const characters = '0123456789abcdef';
+  let hash = '0x';
+
+  for (let i = 0; i < 64; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    hash += characters[randomIndex];
+  }
+
+  return hash;
+};
 
 export const deployRandomApp = async (
   options: {
@@ -157,6 +170,49 @@ export const timeouts = {
   tx: 2 * MAX_EXPECTED_BLOCKTIME,
 };
 
+export function getOrderObject({
+  withDataset,
+  withApp,
+}: { withDataset?: string; withApp?: string } = {}) {
+  return {
+    dataset: withDataset ?? getRandomAddress(),
+    datasetprice: 0,
+    volume: 10,
+    tag: '0x0000000000000000000000000000000000000000000000000000000000000003',
+    apprestrict: withApp ?? getRandomAddress(),
+    workerpoolrestrict: '0x0000000000000000000000000000000000000000',
+    requesterrestrict: '0x0000000000000000000000000000000000000000',
+    salt: '0x2a366726dc6321e78bba6697102f5953ceccfe6c0ddf9499dbb49c99bac1c16d',
+    sign: '0xb00707c4be504e6e07d20bd2e52babd72cbd26f064ec7648c6b684578232bee255a9c98aa2e9b18b4088602967d4f0641d52c0fbb3d5c00304a1f6d3c19eaf4f1c',
+  };
+}
+
+export function getOneDatasetOrder({
+  withDataset,
+  withApp,
+}: { withDataset?: string; withApp?: string } = {}) {
+  return {
+    order: getOrderObject({ withDataset, withApp }),
+    orderHash:
+      '0x396392835c2cbe933023dd28a3d6eedceb21c52b1dba199835a6f24cc75e7685',
+    chainId: 134,
+    publicationTimestamp: '2023-06-15T16:39:22.713Z',
+    signer: '0xD52C27CC2c7D3fb5BA4440ffa825c12EA5658D60',
+    status: 'open',
+    remaining: 10,
+  };
+}
+
+export function resolveWithOneOrder({
+  withDataset,
+  withApp,
+}: { withDataset?: string; withApp?: string } = {}) {
+  return jest.fn<any>().mockResolvedValue({
+    count: 1,
+    orders: [getOneDatasetOrder({ withDataset, withApp })],
+  });
+}
+
 export const MOCK_DATASET_ORDER = {
   orders: [
     {
@@ -242,6 +298,7 @@ export const MOCK_DATASET_ORDER = {
   ],
   count: 4,
 };
+
 export const MOCK_APP_ORDER = {
   orders: [
     {
@@ -323,6 +380,7 @@ export const MOCK_APP_ORDER = {
   ],
   count: 4,
 };
+
 export const MOCK_WORKERPOOL_ORDER = {
   orders: [
     {
@@ -412,10 +470,15 @@ export const MOCK_WORKERPOOL_ORDER = {
   ],
   count: 2,
 };
+
 export const EMPTY_ORDER_BOOK: any = {
   orders: [],
   count: 0,
 };
+
+export function resolveWithNoOrder() {
+  return jest.fn().mockReturnValue(EMPTY_ORDER_BOOK);
+}
 
 export const setBalance = async (
   address: string,
