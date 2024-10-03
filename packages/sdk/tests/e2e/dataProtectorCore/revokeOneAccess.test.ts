5003042452efcba69d92a31a61da419e7d4164f1
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-10-03 10:46:26 +0200

    Unit tests for revokeOneAccess() (#365)

--- packages/sdk/tests/e2e/dataProtectorCore/revokeOneAccess.test.ts
+++ packages/sdk/tests/e2e/dataProtectorCore/revokeOneAccess.test.ts
@@ -2,7 +2,6 @@ import { beforeAll, describe, expect, it } from '@jest/globals';
 import { HDNodeWallet, Wallet } from 'ethers';
 import { IExecDataProtectorCore } from '../../../src/index.js';
 import { ProtectedDataWithSecretProps } from '../../../src/lib/types/index.js';
-import { ValidationError } from '../../../src/utils/errors.js';
 import {
   MAX_EXPECTED_BLOCKTIME,
   MAX_EXPECTED_WEB2_SERVICES_TIME,
@@ -16,6 +15,7 @@ describe('dataProtectorCore.revokeOneAccess()', () => {
   let wallet: HDNodeWallet;
   let protectedData: ProtectedDataWithSecretProps;
   let sconeAppAddress: string;
+
   beforeAll(async () => {
     wallet = Wallet.createRandom();
     dataProtectorCore = new IExecDataProtectorCore(
@@ -45,35 +45,6 @@ describe('dataProtectorCore.revokeOneAccess()', () => {
       const res = await dataProtectorCore.revokeOneAccess(grantedAccess);
       expect(res.access).toStrictEqual(grantedAccess);
       expect(res.txHash).toBeDefined();
-    },
-    MAX_EXPECTED_WEB2_SERVICES_TIME
-  );
-
-  it(
-    'checks arg 0 is a required GrantedAccess',
-    async () => {
-      const undefinedInput: any = undefined;
-      const grantedAccess: any = {
-        dataset: getRandomAddress(),
-        datasetprice: 0,
-        volume: 1,
-        tag: '0x0000000000000000000000000000000000000000000000000000000000000000',
-        apprestrict: getRandomAddress(),
-        workerpoolrestrict: getRandomAddress(),
-        requesterrestrict: getRandomAddress(),
-        salt: '0x0000000000000000000000000000000000000000000000000000000000000000',
-        sign: '0x0000000000000000000000000000000000000000000000000000000000000000',
-      };
-      await expect(
-        dataProtectorCore.revokeOneAccess(undefinedInput)
-      ).rejects.toThrow(
-        new ValidationError('The GrantedAccess is required to be revoked')
-      );
-      await expect(
-        dataProtectorCore.revokeOneAccess({ ...grantedAccess, dataset: 'foo' })
-      ).rejects.toThrow(
-        new ValidationError('dataset should be an ethereum address')
-      );
     },
     MAX_EXPECTED_WEB2_SERVICES_TIME
   );
