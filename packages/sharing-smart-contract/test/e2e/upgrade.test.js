2813cc8ce827f0d1d6fec36277793dcafe6212bd
Zied <26070035+zguesmi@users.noreply.github.com>
2025-05-06 15:16:48 +0200

    Clean

--- packages/sharing-smart-contract/test/e2e/upgrade.test.js
+++ packages/sharing-smart-contract/test/e2e/upgrade.test.js
@@ -1,8 +1,8 @@
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers.js';
 import { expect } from 'chai';
 import hardhat from 'hardhat';
+import { beforeEach } from 'mocha';
 import { createCollection, deploySCFixture } from './utils/loadFixture.test.js';
-import {beforeEach} from 'mocha';
 const { ethers, upgrades } = hardhat;
 
 // TODO convert to ts.
