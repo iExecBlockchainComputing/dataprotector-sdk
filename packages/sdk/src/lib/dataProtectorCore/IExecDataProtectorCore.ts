6c0b33ac7758167562bc2b3185f934ebe729e173
SeddikBellamine <bellamine.seddik@gmail.com>
2025-05-13 11:13:46 +0200

    add init to specific functions

--- packages/sdk/src/lib/dataProtectorCore/IExecDataProtectorCore.ts
+++ packages/sdk/src/lib/dataProtectorCore/IExecDataProtectorCore.ts
@@ -32,6 +32,7 @@ class IExecDataProtectorCore extends IExecDataProtectorModule {
   async protectData(
     args: ProtectDataParams
   ): Promise<ProtectedDataWithSecretProps> {
+    await this.init();
     await isValidProvider(this.iexec);
     return protectData({
       ...args,
@@ -44,21 +45,25 @@ class IExecDataProtectorCore extends IExecDataProtectorModule {
   }
 
   async grantAccess(args: GrantAccessParams): Promise<GrantedAccess> {
+    await this.init();
     await isValidProvider(this.iexec);
     return grantAccess({ ...args, iexec: this.iexec });
   }
 
   async revokeOneAccess(args: GrantedAccess): Promise<RevokedAccess> {
+    await this.init();
     await isValidProvider(this.iexec);
     return revokeOneAccess({ ...args, iexec: this.iexec });
   }
 
   async revokeAllAccess(args: RevokeAllAccessParams): Promise<RevokedAccess[]> {
+    await this.init();
     await isValidProvider(this.iexec);
     return revokeAllAccess({ ...args, iexec: this.iexec });
   }
 
   async transferOwnership(args: TransferParams): Promise<TransferResponse> {
+    await this.init();
     await isValidProvider(this.iexec);
     return transferOwnership({ ...args, iexec: this.iexec });
   }
@@ -66,6 +71,7 @@ class IExecDataProtectorCore extends IExecDataProtectorModule {
   async processProtectedData(
     args: ProcessProtectedDataParams
   ): Promise<ProcessProtectedDataResponse> {
+    await this.init();
     await isValidProvider(this.iexec);
     return processProtectedData({
       ...args,
@@ -74,7 +80,10 @@ class IExecDataProtectorCore extends IExecDataProtectorModule {
   }
 
   // ----- READ METHODS -----
-  getProtectedData(args?: GetProtectedDataParams): Promise<ProtectedData[]> {
+  async getProtectedData(
+    args?: GetProtectedDataParams
+  ): Promise<ProtectedData[]> {
+    await this.init();
     return getProtectedData({
       ...args,
       iexec: this.iexec,
@@ -82,15 +91,17 @@ class IExecDataProtectorCore extends IExecDataProtectorModule {
     });
   }
 
-  getGrantedAccess(
+  async getGrantedAccess(
     args: GetGrantedAccessParams
   ): Promise<GrantedAccessResponse> {
+    await this.init();
     return getGrantedAccess({ ...args, iexec: this.iexec });
   }
 
   async getResultFromCompletedTask(
     args: GetResultFromCompletedTaskParams
   ): Promise<GetResultFromCompletedTaskResponse> {
+    await this.init();
     await isValidProvider(this.iexec);
     return getResultFromCompletedTask({
       ...args,
