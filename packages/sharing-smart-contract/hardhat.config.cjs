360fe70828fe1c9575a2dc50bc7fcc1d7b7724ef
Le-Caignec <robinlecaignec@yahoo.fr>
2025-07-29 01:02:47 +0200

    fix: update salt value in create2 strategy configuration

--- packages/sharing-smart-contract/hardhat.config.cjs
+++ packages/sharing-smart-contract/hardhat.config.cjs
@@ -142,7 +142,7 @@ module.exports = {
     strategyConfig: {
         //TODO: Handle salt dynamically depending on the network and the dev/prod environment
       create2: {
-        salt: "0x0100001000010000100010000100000000100001000000000100000000000003",
+        salt: "0x0100001000010000100010000100000000100001000000000100000000000004",
       },
     },
   },
