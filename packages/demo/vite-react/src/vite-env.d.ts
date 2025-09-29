063381a3034959d9864d5bfb26438530c0a23576
Le-Caignec <robinlecaignec@yahoo.fr>
2025-09-29 13:55:49 +0200

    remove demo

--- packages/demo/vite-react/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-import { MetaMaskInpageProvider } from '@metamask/providers';
-
-/// <reference types="vite/client" />
-
-declare global {
-  interface Window {
-    ethereum: MetaMaskInpageProvider;
-  }
-}
