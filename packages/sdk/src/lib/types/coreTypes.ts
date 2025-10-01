9b4a9784f5ac472e116a4986f1b9da151cd74624
SeddikBellamine <bellamine.seddik@gmail.com>
2025-10-01 12:43:33 +0200

    feat: add result encryption types to processProtectedData
    - Add encryptResult and pemPrivateKey parameters to ProcessProtectedDataParams
    - Add PUSH_ENCRYPTION_KEY status to ProcessProtectedDataStatuses
    - Add pemPrivateKey to ProcessProtectedDataResponse and GetResultFromCompletedTaskResponse

--- packages/sdk/src/lib/types/coreTypes.ts
+++ packages/sdk/src/lib/types/coreTypes.ts
@@ -223,6 +223,7 @@ export type GetResultFromCompletedTaskParams = {
 
 export type GetResultFromCompletedTaskResponse = {
   result: ArrayBuffer;
+  pemPrivateKey?: string;
 };
 
 // ---------------------RevokeAccess Types------------------------------------
@@ -280,6 +281,7 @@ export type ProcessProtectedDataStatuses =
   | 'FETCH_APP_ORDERBOOK'
   | 'FETCH_WORKERPOOL_ORDERBOOK'
   | 'PUSH_REQUESTER_SECRET'
+  | 'PUSH_ENCRYPTION_KEY'
   | 'REQUEST_TO_PROCESS_PROTECTED_DATA'
   | 'CONSUME_TASK'
   | 'CONSUME_RESULT_DOWNLOAD'
@@ -356,6 +358,17 @@ export type ProcessProtectedDataParams = {
   voucherOwner?: AddressOrENS;
 
   /**
+   * Enable result encryption for the processed data.
+   * @default = false
+   */
+  encryptResult?: boolean;
+
+  /**
+   * Private key in PEM format for result encryption/decryption.
+   * If not provided and encryptResult is true, a new key pair will be generated.
+   */
+  pemPrivateKey?: string;
+  /**
    * Callback function that will get called at each step of the process
    */
   onStatusUpdate?: OnStatusUpdateFn<ProcessProtectedDataStatuses>;
@@ -366,4 +379,5 @@ export type ProcessProtectedDataResponse = {
   dealId: string;
   taskId: string;
   result: ArrayBuffer;
+  pemPrivateKey?: string;
 };
