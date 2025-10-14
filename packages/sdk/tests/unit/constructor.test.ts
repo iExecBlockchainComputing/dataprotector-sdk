01270443030f208b125f28690b9f43abf0875d2a
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-14 12:00:34 +0200

    fix: update iexec version to 8.20.0 and skip experimental network tests

--- packages/sdk/tests/unit/constructor.test.ts
+++ packages/sdk/tests/unit/constructor.test.ts
@@ -189,7 +189,7 @@ describe('IExecDataProtector()', () => {
     });
   });
 
-  describe('When instantiating SDK with an experimental network', () => {
+  describe.skip('When instantiating SDK with an experimental network', () => {
     const experimentalNetworkSigner = getWeb3Provider(
       Wallet.createRandom().privateKey,
       {
