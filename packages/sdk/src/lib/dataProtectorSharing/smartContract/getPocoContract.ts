eda49e7d0b0929c57ecbdd70856ec4d3d95a374e
SeddikBellamine <bellamine.seddik@gmail.com>
2025-12-05 06:32:15 +0100

    refactor(sdk): remove dataprotector-sharing module source code

--- packages/sdk/src/lib/dataProtectorSharing/smartContract/getPocoContract.ts
+++
@@ -1,7 +0,0 @@
-import type { IExecModule } from 'iexec';
-import type { IPoCo } from '../../../../generated/typechain/sharing/interfaces/IPoCo.js';
-
-export async function getPocoContract(iexec: IExecModule): Promise<IPoCo> {
-  const client = await iexec.config.resolveContractsClient();
-  return client.getIExecContract() as unknown as IPoCo;
-}
